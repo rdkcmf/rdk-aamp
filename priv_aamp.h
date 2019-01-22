cc591213713a440d866b720603f9a82a55c4338e
Christo Joseph <Christo_Joseph@comcast.com>
2019-01-22 19:51:41 +0530

    DELIA-29015 - Log flooding on iframe trickmodes
    Reason for change: Iframe latency logging flag is enabled unintentionally
    since it is out of array boundary, so increase size of the array to
    contain iframe type as well. Also added newline to latency logging.
    Risks: Low
    Test Procedure: Tested on xi5.
    Signed-off-by: Christo Joseph <Christo_Joseph@comcast.com>
    
    Change-Id: I594990562873fb254f2c15151bd06454c97ea5ad

--- priv_aamp.h
+++ priv_aamp.h
@@ -231,7 +231,7 @@ enum HttpHeaderType
 /**
  * @brief maximum supported mediatype for latency logging
  */
-#define MAX_SUPPORTED_LATENCY_LOGGING_TYPES	4
+#define MAX_SUPPORTED_LATENCY_LOGGING_TYPES	(eMEDIATYPE_IFRAME+1)
 
 /**
  * @brief Log level's of AAMP
