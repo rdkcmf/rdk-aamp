rdkcmf/rdk-aamp

commit 6fd33c53f0547c45b42b2ff11ae91e1eb43c88ba

DELIA-32951 - Deprecate use of GrowableBuffer
Reason for change: Use Content-Length HTTP header  to set buffer size of
download buffers rather than doing memory reallocations.
Risks: Low
Test Procedure: Tested on pacexi5.
Signed-off-by: Christo Joseph <Christo_Joseph@comcast.com>

Change-Id: I71d32266976354428da7904337da53486846bba3

main_aamp.cpp
@@ -1255,7 +1255,7 @@ void aamp_AppendBytes(struct GrowableBuffer *buffer, const void *ptr, size_t len
 	{
 		if(buffer->avail > (128*1024))
 		{
-			AAMPLOG_INFO("%s:%d realloc. buf %p avail %d required %d\n", __FUNCTION__, __LINE__, buffer, (int)buffer->avail, (int)required);
+			logprintf("%s:%d WARNING - realloc. buf %p avail %d required %d\n", __FUNCTION__, __LINE__, buffer, (int)buffer->avail, (int)required);
 		}
 		buffer->avail = required * 2; // grow generously to minimize realloc overhead
 		char *ptr = (char *)g_realloc(buffer->ptr, buffer->avail);
@@ -1276,13 +1276,14 @@ void aamp_AppendBytes(struct GrowableBuffer *buffer, const void *ptr, size_t len
 }
 
 /**
- * @struct WriteContext
- * @brief context during curl write callback
+ * @struct CurlCallbackContext
+ * @brief context during curl callbacks
  */
-struct WriteContext
+struct CurlCallbackContext
 {
 	PrivateInstanceAAMP *aamp;
 	GrowableBuffer *buffer;
+	httpRespHeaderData *responseHeaderData;
 };
 
 /**
@@ -1290,13 +1291,13 @@ struct WriteContext
  * @param ptr pointer to buffer containing the data
  * @param size size of the buffer
  * @param nmemb number of bytes
- * @param userdata WriteContext pointer
+ * @param userdata CurlCallbackContext pointer
  * @retval size consumed or 0 if interrupted
  */
 static size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata)
 {
 	size_t ret = 0;
-	struct WriteContext *context = (struct WriteContext *)userdata;
+	CurlCallbackContext *context = (CurlCallbackContext *)userdata;
 	pthread_mutex_lock(&context->aamp->mLock);
 	if (context->aamp->mDownloadsEnabled)
 	{
@@ -1314,17 +1315,18 @@ static size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdat
 
 
 /**
- * @brief
- * @param ptr
- * @param size
- * @param nmemb
- * @param user_data
+ * @brief callback invoked on http header by curl
+ * @param ptr pointer to buffer containing the data
+ * @param size size of the buffer
+ * @param nmemb number of bytes
+ * @param user_data  CurlCallbackContext pointer
  * @retval
  */
 static size_t header_callback(void *ptr, size_t size, size_t nmemb, void *user_data)
 {
 	//std::string *httpHeaders = static_cast<std::string *>(user_data);
-	httpRespHeaderData *httpHeader = static_cast<httpRespHeaderData *>(user_data);
+	CurlCallbackContext *context = static_cast<CurlCallbackContext *>(user_data);
+	httpRespHeaderData *httpHeader = context->responseHeaderData;
 	size_t len = nmemb * size;
 	int startPos = 0;
 	int endPos = 0;
@@ -1343,6 +1345,27 @@ static size_t header_callback(void *ptr, size_t size, size_t nmemb, void *user_d
 		httpHeader->type = eHTTPHEADERTYPE_COOKIE;
 		startPos = header.find("Set-Cookie:") + strlen("Set-Cookie:");
 		endPos = header.length() - 1;
+	}
+	else
+	{
+		size_t headerStart = header.find("Content-Length:");
+		if (std::string::npos != headerStart )
+		{
+			int contentLengthStartPosition = headerStart + (sizeof("Content-Length:")-1);
+			while (isspace(header[contentLengthStartPosition]) && (contentLengthStartPosition <= header.length()))
+			{
+				contentLengthStartPosition++;
+			}
+			while(isspace(header.back()))
+			{
+				header.pop_back();
+			}
+			std::string contentLengthStr = header.substr(contentLengthStartPosition);
+			int contentLength = std::stoi(contentLengthStr);
+			traceprintf("%s:%d header %s contentLengthStr %s  contentLength %d\n",__FUNCTION__,__LINE__, header.c_str(), contentLengthStr.c_str(), contentLength);
+			/*Add 2 additional characters to take care of extra characters inserted by aamp_AppendNulTerminator*/
+			aamp_Malloc(context->buffer, contentLength+2);
+		}
 	}
 
 	if((startPos > 0) && (endPos > 0))
@@ -1469,8 +1492,6 @@ void PrivateInstanceAAMP::CurlInit(int startIdx, unsigned int instanceCount)
 			curl_easy_setopt(curl[i], CURLOPT_PROGRESSDATA, this);
 			curl_easy_setopt(curl[i], CURLOPT_PROGRESSFUNCTION, progress_callback);
 			curl_easy_setopt(curl[i], CURLOPT_HEADERFUNCTION, header_callback);
-			//curl_easy_setopt(curl[i], CURLOPT_HEADERDATA, &cookieHeaders[i]);
-			curl_easy_setopt(curl[i], CURLOPT_HEADERDATA, &httpRespHeaders[i]);
 			curl_easy_setopt(curl[i], CURLOPT_WRITEFUNCTION, write_callback);
 			curl_easy_setopt(curl[i], CURLOPT_TIMEOUT, DEFAULT_CURL_TIMEOUT);
 			curl_easy_setopt(curl[i], CURLOPT_CONNECTTIMEOUT, DEFAULT_CURL_CONNECTTIMEOUT);
@@ -1768,10 +1789,12 @@ bool PrivateInstanceAAMP::GetFile(const char *remoteUrl, struct GrowableBuffer *
 		if (curl)
 		{
 			curl_easy_setopt(curl, CURLOPT_URL, remoteUrl);
-			struct WriteContext context;
+			CurlCallbackContext context;
 			context.aamp = this;
 			context.buffer = buffer;
+			context.responseHeaderData = &httpRespHeaders[curlInstance];
 			curl_easy_setopt(curl, CURLOPT_WRITEDATA, &context);
+			curl_easy_setopt(curl, CURLOPT_HEADERDATA, &context);
 			curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
 
 			// note: win32 curl lib doesn't support multi-part range

StreamAbstractionAAMP.h
@@ -294,7 +294,6 @@ private:
 	int fragmentIdxToFetch;             /**< Read position */
 	int bandwidthBytesPerSecond;        /**< Bandwidth of last selected profile*/
 	double totalFetchedDuration;        /**< Total fragment fetched duration*/
-	size_t fetchBufferPreAllocLen;      /** Buffer length to pre-allocate for next fetch buffer*/
 	bool discontinuityProcessed;
 
 	BufferHealthStatus bufferStatus;     /**< Buffer status of the track*/

streamabstraction.cpp
@@ -202,18 +202,6 @@ void MediaTrack::UpdateTSAfterFetch()
 	}
 #endif
 	totalFetchedDuration += cachedFragment[fragmentIdxToFetch].duration;
-	size_t fragmentLen = cachedFragment[fragmentIdxToFetch].fragment.len;
-	if (fetchBufferPreAllocLen < fragmentLen)
-	{
-		logprintf("%s:%d [%s] Update fetchBufferPreAllocLen[%u]->[%u]\n", __FUNCTION__, __LINE__,
-		        name, fetchBufferPreAllocLen, fragmentLen);
-		fetchBufferPreAllocLen = fragmentLen;
-	}
-	else
-	{
-		traceprintf("%s:%d [%s] fetchBufferPreAllocLen[%u] fragment.len[%u] diff %u\n", __FUNCTION__, __LINE__,
-		        name, fetchBufferPreAllocLen, fragmentLen, (fetchBufferPreAllocLen - fragmentLen));
-	}
 
 	if((eTRACK_VIDEO == type) && aamp->IsFragmentBufferingRequired())
 	{
@@ -674,17 +662,6 @@ CachedFragment* MediaTrack::GetFetchBuffer(bool initialize)
 			logprintf("%s:%d fragment.ptr already set - possible memory leak\n", __FUNCTION__, __LINE__);
 		}
 		memset(&cachedFragment->fragment, 0x00, sizeof(GrowableBuffer));
-		double duration = (0 == fragmentDurationSeconds)?(2.0):fragmentDurationSeconds;
-		size_t estimatedFragmentSizeFromBW = bandwidthBytesPerSecond * duration * 1.5;
-		if (estimatedFragmentSizeFromBW > fetchBufferPreAllocLen)
-		{
-			logprintf("%s:%d [%s] Update fetchBufferPreAllocLen[%u]->[%u]\n", __FUNCTION__, __LINE__,
-			        name, fetchBufferPreAllocLen, estimatedFragmentSizeFromBW);
-			fetchBufferPreAllocLen = estimatedFragmentSizeFromBW;
-		}
-		traceprintf ("%s:%d [%s] bandwidthBytesPerSecond %d fragmentDurationSeconds %f fetchBufferPreAllocLen %d, estimatedFragmentSizeFromBW %d\n",
-				__FUNCTION__, __LINE__, name, bandwidthBytesPerSecond, fragmentDurationSeconds, fetchBufferPreAllocLen, estimatedFragmentSizeFromBW);
-		aamp_Malloc(&cachedFragment->fragment, fetchBufferPreAllocLen);
 	}
 	return cachedFragment;
 }
@@ -721,7 +698,7 @@ MediaTrack::MediaTrack(TrackType type, PrivateInstanceAAMP* aamp, const char* na
 		fragmentInjectorThreadStarted(false), totalInjectedDuration(0), cacheDurationSeconds(0),
 		notifiedCachingComplete(false), fragmentDurationSeconds(0), segDLFailCount(0),segDrmDecryptFailCount(0),mSegInjectFailCount(0),
 		bufferStatus(BUFFER_STATUS_GREEN), prevBufferStatus(BUFFER_STATUS_GREEN), bufferHealthMonitorIdleTaskId(0),
-		bandwidthBytesPerSecond(AAMP_DEFAULT_BANDWIDTH_BYTES_PREALLOC), totalFetchedDuration(0), fetchBufferPreAllocLen(0),
+		bandwidthBytesPerSecond(AAMP_DEFAULT_BANDWIDTH_BYTES_PREALLOC), totalFetchedDuration(0),
 		discontinuityProcessed(false), ptsError(false), cachedFragment(NULL)
 {
 	this->type = type;

commit 98bef229a6279345f2ef7d6b63a83b85f66daf23

DELIA-32951 - [AAMP] deprecate use of GrowableBuffer
Reason for change: For redirects, content length header is zero and
results in assertion failure. Regression introduced by previous commit
6fd33c53f0547c45b42b2ff11ae91e1eb43c88ba
Risks: Low
Test Procedure: Tested on pacexi5.
Signed-off-by: Christo Joseph <Christo_Joseph@comcast.com>

Change-Id: I72d32300ffeb66b22a27ee3388d96348469a854a

main_aamp.cpp
@@ -1363,8 +1363,12 @@ static size_t header_callback(void *ptr, size_t size, size_t nmemb, void *user_d
 			std::string contentLengthStr = header.substr(contentLengthStartPosition);
 			int contentLength = std::stoi(contentLengthStr);
 			traceprintf("%s:%d header %s contentLengthStr %s  contentLength %d\n",__FUNCTION__,__LINE__, header.c_str(), contentLengthStr.c_str(), contentLength);
-			/*Add 2 additional characters to take care of extra characters inserted by aamp_AppendNulTerminator*/
-			aamp_Malloc(context->buffer, contentLength+2);
+			/*contentLength can be zero for redirects*/
+			if (contentLength > 0)
+			{
+				/*Add 2 additional characters to take care of extra characters inserted by aamp_AppendNulTerminator*/
+				aamp_Malloc(context->buffer, contentLength + 2);
+			}
 		}
 	}
 