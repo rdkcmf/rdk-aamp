a8f03831ef93fc6095939c7ef1f96a5b02b50791
Christo Joseph <Christo_Joseph@comcast.com>
2019-03-04 19:16:01 +0530

    DELIA-33554 - No Audio in linear channel Rogers
    Reason for change: Disabling audio track's discontinuity can cause freezes
    as video injection is stopped and audio injection continues. If multiple
    tracks are present, ignore discontinuity tag present in a track only if other
    track has no discontinuity around the same time.
    
    Risks: Low
    Test Procedure: Tested by simulation by introducing discontinuity tag in
    audio track on a locally hosted stream. Also verified regression on VSS
    stream due to disabling audio discontinuity completely is fixed.
    Signed-off-by: Christo Joseph <Christo_Joseph@comcast.com>
    
    Change-Id: Ia00345994da976280d84736c5aadc7c9ff7b8806

--- fragmentcollector_hls.cpp
+++ fragmentcollector_hls.cpp
@@ -65,6 +65,7 @@
 #define DRM_IV_LEN 16
 #define MAX_LICENSE_ACQ_WAIT_TIME 10000  // 10 secs
 #define MAX_SEQ_NUMBER_LAG_COUNT 50 /* Configured sequence number max count to avoid continuous looping for an edge case scenario, which leads crash due to hung */
+#define DISCONTINUITY_DISCARD_TOLERANCE_SECONDS 30 /* Used by discontinuity handling logic to ensure both tracks have discontinuity tag around same area*/
 
 pthread_mutex_t gDrmMutex = PTHREAD_MUTEX_INITIALIZER;
 static unsigned char gDeferredDrmMetaDataSha1Hash[DRM_SHA1_HASH_LEN]; /**Sha1 hash of meta-data for deferred DRM license acquisition*/
@@ -993,10 +994,14 @@ char *TrackState::GetNextFragmentUriFromPlaylist()
 				else if (startswith(&ptr, "-X-DISCONTINUITY"))
 				{
 					logprintf("#EXT-X-DISCONTINUITY in track[%d]\n", type);
-					if(type == 0)
+					discontinuity = true;
+
+					TrackType otherType = (type == eTRACK_VIDEO)? eTRACK_AUDIO: eTRACK_VIDEO;
+					TrackState *other = context->trackState[otherType];
+					if (other->enabled && !other->HasDiscontinuityAroundPosition(playTarget))
 					{
-						logprintf("#EXT-X-DISCONTINUITY in track[%d]\n", type);
-						discontinuity = true;
+						logprintf("Ignoring discontinuity as %s track does not have discontinuity\n", other->name);
+						discontinuity = false;
 					}
 				}
 				else if (startswith(&ptr, "-X-I-FRAMES-ONLY"))
@@ -4352,6 +4357,39 @@ int TrackState::GetNumberOfPeriods()
 	return (int)mPeriodPositionIndex.size();
 }
 
+/***************************************************************************
+* @fn HasDiscontinuityAroundPosition
+* @brief Check if discontinuity present around given position
+* @param[in] position Position to check for discontinuity
+* @return true if discontinuity present around given position
+***************************************************************************/
+bool TrackState::HasDiscontinuityAroundPosition(double position)
+{
+	bool discontinuityPending = false;
+	pthread_mutex_lock(&mutex);
+	if (0 != mPeriodPositionIndex.size())
+	{
+		double low = position - DISCONTINUITY_DISCARD_TOLERANCE_SECONDS;
+		double high = position + DISCONTINUITY_DISCARD_TOLERANCE_SECONDS;
+		std::map<int, double>::iterator it = mPeriodPositionIndex.begin();
+		while (it != mPeriodPositionIndex.end())
+		{
+			traceprintf("%s:%d low %f high %f position %f discontinuity %f\n", __FUNCTION__, __LINE__, low, high, position, it->second);
+			if ( low < it->second && high > it->second )
+			{
+				discontinuityPending = true;
+				break;
+			}
+			it++;
+		}
+		if (!discontinuityPending)
+		{
+			logprintf("%s:%d Discontinuity not found in window low %f high %f position %f\n", __FUNCTION__, __LINE__, low, high, position);
+		}
+	}
+	pthread_mutex_unlock(&mutex);
+	return discontinuityPending;
+}
 
 /***************************************************************************
 * @brief Fetch init fragment for fragmented mp4 format
