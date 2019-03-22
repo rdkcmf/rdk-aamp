28877965ad86e55977fd81f891daf1858928664f
Navaneeth H <navaneeth.h@tataelxsi.co.in>
2019-03-22 11:10:40 +0530

    DELIA-33921:[AAMP] Parse error in "Spring_4Ktest.mpd"
    Reason for change: Add support for SegmentBase MPDs with no initialization tag.
    Risks: Low
    Test Procedure: Playback the stream mentioned in the jira ticket
    Signed-off-by: Navaneeth H <navaneeth.h@tataelxsi.co.in>
    
    Change-Id: I58a25ccd71f27333347fa366560631a832f6020d

--- fragmentcollector_mpd.cpp
+++ fragmentcollector_mpd.cpp
@@ -1785,7 +1785,15 @@ double PrivateStreamAbstractionMPD::SkipFragments( MediaStreamContext *pMediaStr
 		}
 		else
 		{
-			aamp_Error("not-yet-supported mpd format");
+			ISegmentBase *segmentBase = pMediaStreamContext->representation->GetSegmentBase();
+			if(segmentBase)
+			{
+				skipTime=0;
+			}
+			else
+			{
+				aamp_Error("not-yet-supported mpd format");
+			}
 		}
 	}
 	return skipTime;
