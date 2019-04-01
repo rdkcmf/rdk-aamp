6477928226e24fdcacf53e9205fbe1e9ebbf3e0b
Philip <philip_stroffolino@cable.comcast.com>
2019-04-01 14:30:57 +0000

    Revert "Revert "CPC-2365:Eliminate need for patches required to get AAMP to build in RDKX""
    This reverts commit 7f9e0c70f83b5d71eeda1b3da9c12392e0425a5d.
    
    Change-Id: I2245c6799c43a86ea5c8d22afbdb5ce4b627a47b

--- drm/aampoutputprotection.cpp
+++ drm/aampoutputprotection.cpp
@@ -61,11 +61,13 @@ AampOutputProtection::AampOutputProtection()
     // Get initial HDCP status
     SetHDMIStatus();
 
+#ifdef RDK_VIDEO_BUILD
     // Register IARM callbacks
     logprintf("%s : registering DSMGR events...\n", __FUNCTION__);
     IARM_Bus_RegisterEventHandler(IARM_BUS_DSMGR_NAME,IARM_BUS_DSMGR_EVENT_HDMI_HOTPLUG, HDMIEventHandler);
     IARM_Bus_RegisterEventHandler(IARM_BUS_DSMGR_NAME,IARM_BUS_DSMGR_EVENT_HDCP_STATUS, HDMIEventHandler);
     IARM_Bus_RegisterEventHandler(IARM_BUS_DSMGR_NAME,IARM_BUS_DSMGR_EVENT_RES_POSTCHANGE, ResolutionHandler);
+#endif // RDK_VIDEO_BUILD
 }
 
 /**
@@ -75,11 +77,13 @@ AampOutputProtection::~AampOutputProtection()
 {
     DEBUG_FUNC;
 
+#ifdef RDK_VIDEO_BUILD
     // Register IARM callbacks
     logprintf("%s : unregistering DSMGR events...\n", __FUNCTION__);
     IARM_Bus_RemoveEventHandler(IARM_BUS_DSMGR_NAME,IARM_BUS_DSMGR_EVENT_HDMI_HOTPLUG, HDMIEventHandler);
     IARM_Bus_RemoveEventHandler(IARM_BUS_DSMGR_NAME,IARM_BUS_DSMGR_EVENT_HDCP_STATUS, HDMIEventHandler);
     IARM_Bus_RemoveEventHandler(IARM_BUS_DSMGR_NAME,IARM_BUS_DSMGR_EVENT_RES_POSTCHANGE, ResolutionHandler);
+#endif // RDK_VIDEO_BUILD
 
     s_pAampOP = NULL;
 
@@ -196,6 +200,7 @@ bool AampOutputProtection::IsSourceUHD()
  */
 void AampOutputProtection::SetHDMIStatus()
 {
+#ifdef RDK_VIDEO_BUILD
     bool                    isConnected              = false;
     bool                    isHDCPCompliant          = false;
     bool                    isHDCPEnabled            = true;
@@ -245,6 +250,12 @@ void AampOutputProtection::SetHDMIStatus()
         m_hdcpCurrentProtocol = dsHDCP_VERSION_1X;
         logprintf("%s : GetHDCPVersion: Did not detect HDCP version defaulting to 1.4 (%d)\n", __FUNCTION__, m_hdcpCurrentProtocol);
     }
+#else
+	// No video output on device mark HDCP protection as valid
+	m_hdcpCurrentProtocol = dsHDCP_VERSION_1X;
+	m_isHDCPEnabled = true;
+	return;
+#endif // RDK_VIDEO_BUILD
 
     return;
 }
@@ -339,6 +350,7 @@ DRM_RESULT DRM_CALL AampOutputProtection::PR_OP_Callback(const DRM_VOID *f_pvOut
 #endif
 
 
+#ifdef RDK_VIDEO_BUILD
 /**
  * @brief IARM event handler for HDCP and HDMI hot plug events
  * @param owner
@@ -424,6 +436,7 @@ void AampOutputProtection::ResolutionHandler(const char *owner, IARM_EventId_t e
 
     pInstance->Release();
 }
+#endif // RDK_VIDEO_BUILD
 
 
 /**
