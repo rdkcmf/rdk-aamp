16edbf2133328c9c9e04132b34042439771bec91
Paulpandian <paulpandian_mariappan@cable.comcast.com>
2019-05-30 18:17:35 -0400

    DELIA-35629 [DASH][AAMP] SIGSEGV (MediaTrack::InjectFragment) Crash during LS.
    Reason for change: During trickplay, mMediaStreamContext will not be allocated
    for audio track. Here, in a race condition discontinuity processed
    very immediate after play rate has been changed to Normal before tune was completed.
    So, during that time de-referring invalid audio stream context for checking
    track presence caused the crash here.
    Test Procedure: Do trick play with lightning and normal speed to verify the issue.
    Risks: None
    Signed-off-by: Paulpandian M <paulpandian_mariappan@cable.comcast.com>
    
    Change-Id: Ia244847811daf56a473e072d19c7ad5a062ec172
    (cherry picked from commit 25175e994cfba940dc5ccb5f6217266deb4275f1)
    (cherry picked from commit b1c85733f5482ccdca1063513367adc75391a940)
    (cherry picked from commit 14fb98e3eb0ff213b8590baf2ff046b30b1966df)
    (cherry picked from commit 04aef5534b80dff8ab8083507e962b7b1975e558)

--- streamabstraction.cpp
+++ streamabstraction.cpp
@@ -423,7 +423,7 @@ bool MediaTrack::InjectFragment()
 #endif
 		if (cachedFragment->fragment.ptr)
 		{
-			StreamAbstractionAAMP*  context = GetContext();
+			StreamAbstractionAAMP* context = GetContext();
 #ifdef AAMP_DEBUG_INJECT
 			if ((1 << type) & AAMP_DEBUG_INJECT)
 			{
@@ -447,15 +447,19 @@ bool MediaTrack::InjectFragment()
 			}
 			else if ((cachedFragment->discontinuity || ptsError) && (AAMP_NORMAL_PLAY_RATE == context->aamp->rate))
 			{
-				logprintf("%s:%d - track %s- notifying aamp discontinuity\n", __FUNCTION__, __LINE__, name);
+				logprintf("%s:%d - track %s - notifying aamp discontinuity\n", __FUNCTION__, __LINE__, name);
 				cachedFragment->discontinuity = false;
 				ptsError = false;
 				stopInjection = aamp->Discontinuity((MediaType) type);
+
 				/*For muxed streams, give discontinuity for audio track as well*/
-				if (!context->GetMediaTrack(eTRACK_AUDIO)->enabled)
+				MediaTrack* audio = context->GetMediaTrack(eTRACK_AUDIO);
+
+				if (audio && !audio->enabled)
 				{
 					aamp->Discontinuity(eMEDIATYPE_AUDIO);
 				}
+
 				if (stopInjection)
 				{
 					ret = false;
@@ -471,6 +475,7 @@ bool MediaTrack::InjectFragment()
 			{
 				SignalTrickModeDiscontinuity();
 			}
+
 			if (!stopInjection)
 			{
 #ifdef AAMP_DEBUG_INJECT
@@ -1309,7 +1314,6 @@ bool StreamAbstractionAAMP::UpdateProfileBasedOnFragmentCache()
  */
 void StreamAbstractionAAMP::CheckForPlaybackStall(bool fragmentParsed)
 {
-	MediaTrack *video = GetMediaTrack(eTRACK_VIDEO);
 	if (fragmentParsed)
 	{
 		mLastVideoFragParsedTimeMS = aamp_GetCurrentTimeMS();
