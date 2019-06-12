ad12699c36b393e0c6ca2d821448cb29e69e8a64
Christo Joseph <Christo_Joseph@comcast.com>
2019-06-12 16:23:08 +0530

    DELIA-36337 - srcpad leak in AAMPGstPlayer::SignalTrickModeDiscontinuity()
    Reason for change: Release reference of srcpad obtained by
    gst_element_get_static_pad
    Risks: Low
    Test Procedure: Tested on pacexi5.
    Signed-off-by: Christo Joseph <Christo_Joseph@comcast.com>
    
    Change-Id: Id41a288522d62a64a0629907beb17e73d63ebca3

--- aampgstplayer.cpp
+++ aampgstplayer.cpp
@@ -2985,6 +2985,7 @@ void AAMPGstPlayer::SignalTrickModeDiscontinuity()
 		{
 			logprintf("%s:%d Sent aamp-tm-disc event\n", __FUNCTION__, __LINE__);
 		}
+		gst_object_unref(sourceEleSrcPad);
 	}
 }
 
