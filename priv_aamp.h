37fc4ab65aec36db1aeba18febabe1aa3d075ad8
Philip <philip_stroffolino@cable.comcast.com>
2019-06-18 23:11:38 +0100

    Revert "DELIA-35144 : DRM timeout"
    This reverts commit 8958809dc5dfbc3795d30e2fd2fd776be708b23a.
    
    Change-Id: I995b8d48ea59897564a511f1c3ef78dfa44241cb
    (cherry picked from commit 8824dc9fa899a0cd0cf79219b86c016f4490fd35)

--- priv_aamp.h
+++ priv_aamp.h
@@ -1440,7 +1440,6 @@ public:
 	VideoZoomMode zoom_mode;
 	bool video_muted;
 	int audio_volume;
-	bool ave_adapter;
 	std::vector<std::string> subscribedTags;
 	std::vector<TimedMetadata> timedMetadata;
 
