e608707e1906ea74ade5b54ecbbb896616063e1e
Manuchand K S <MANUCHAND_SUNILKUMAR@comcast.com>
2019-06-27 18:18:48 +0530

    DELIA-35513 : video tag version limitations/bugs
    Reason for change: Resolve video tag issues.
    
    Test Procedure: Launched the player and verified the functionalities for
    UVE and HTML Players.
    
    Risks: Low
    
    Change-Id: Id4ec5ccd30e6350d31c28864d267c02e56e95d43
    Signed-off-by: Manuchand K S <MANUCHAND_SUNILKUMAR@comcast.com>
    (cherry picked from commit 3f55e5ffa2009531290a4b264e1d078844227727)

--- test/ReferencePlayer/URLs.js
+++ test/ReferencePlayer/URLs.js
@@ -20,6 +20,6 @@
 var urls = [
      { name:"Big Buck Bunny (CLEAR)", url: "http://amssamples.streaming.mediaservices.windows.net/683f7e47-bd83-4427-b0a3-26a6c4547782/BigBuckBunny.ism/manifest(format=mpd-time-csf)", useComcastDrmConfig: false },
      { name:"Transformers (DASH)", url: "http://ccr.ipvod-t6.xcr.comcast.net/ipvod11/TVNC0000000003007630/movie/1558999503868/manifest.mpd", useComcastDrmConfig: true },
-     { name:"Captain America (HLS)", url: "http://ccr.ipvod-t6.xcr.comcast.net/ipvod20/PCKG4033105720190601/movie/1558583414729/manifest.m3u8", useComcastDrmConfig: true }, 
+     { name:"Deadpool (HLS)", url: "http://ccr.ipvod-t6.xcr.comcast.net/ipvod4/PFXM3033039820190601/movie/1558895446379/manifest.m3u8", useComcastDrmConfig: true },
      { name:"Godzilla (HLS)", url: "http://ccr.ipvod-t6.xcr.comcast.net/ipvod1/TVNC0000000002950377/movie/1556381264464/manifest.m3u8", useComcastDrmConfig: true }
 ];
