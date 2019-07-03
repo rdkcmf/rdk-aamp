fecb3374fc52afab5f402178f869c264708ba5d7
Paulpandian <paulpandian_mariappan@cable.comcast.com>
2019-07-03 10:46:26 -0400

    DELIA-36739 AAMP/UVE crashes rdk browser when inserting specific ad
    Reason for change: This particular manifest does not have LANGUAGE tag in it,
    so in this case de-referring null address crashes.
    Fixed by adding check befor de-referring this->mediaInfo[i].language
    on GetPlaylistURI()
    Test Procedure: Tested by simulation by tuning to the particular stream
    Risks: None
    Signed-off-by: Paulpandian M <paulpandian_mariappan@cable.comcast.com>
    
    Change-Id: I7edb4f78d2b021608e44916e37af86aba30e9773
    (cherry picked from commit 4c94591fda25b96c81b47b75cf8b25d5d2e7323e)

--- fragmentcollector_hls.cpp
+++ fragmentcollector_hls.cpp
@@ -2552,6 +2552,9 @@ void TrackState::RefreshPlaylist(void)
 const char *StreamAbstractionAAMP_HLS::GetPlaylistURI(TrackType trackType, StreamOutputFormat* format)
 {
 	const char *playlistURI = NULL;
+	const char* group = NULL;
+	HlsStreamInfo* streamInfo = NULL;
+
 	switch (trackType)
 	{
 	case eTRACK_VIDEO:
@@ -2564,8 +2567,8 @@ const char *StreamAbstractionAAMP_HLS::GetPlaylistURI(TrackType trackType, Strea
 
 	case eTRACK_AUDIO:
 		assert(GetProfileCount() > this->currentProfileIndex);
-		HlsStreamInfo* streamInfo = &this->streamInfo[this->currentProfileIndex];
-		const char* group = streamInfo->audio;
+		streamInfo = &this->streamInfo[this->currentProfileIndex];
+		group = streamInfo->audio;
 		if (group)
 		{
 			logprintf("GetPlaylistURI : AudioTrack: group %s, aamp->language %s\n", group, aamp->language);
@@ -2583,7 +2586,7 @@ const char *StreamAbstractionAAMP_HLS::GetPlaylistURI(TrackType trackType, Strea
 //	#ifdef TRACE
 						logprintf("GetPlaylistURI checking if preferred language '%s' matches media[%d] language '%s'\n", aamp->language, i, this->mediaInfo[i].language);
 //	#endif
-						if ( (aamp->language[0] && strncmp(aamp->language, this->mediaInfo[i].language, MAX_LANGUAGE_TAG_LENGTH)==0) || (langChecks == 1 && this->mediaInfo[i].isDefault) )
+						if ((aamp->language[0] && this->mediaInfo[i].language && strncmp(aamp->language, this->mediaInfo[i].language, MAX_LANGUAGE_TAG_LENGTH)==0) || (langChecks == 1 && this->mediaInfo[i].isDefault))
 						{
 							foundAudio = true;
 							if(langChecks == 1)
@@ -2591,7 +2594,10 @@ const char *StreamAbstractionAAMP_HLS::GetPlaylistURI(TrackType trackType, Strea
 								//save what language we have selected, defaulting to english
 								logprintf("%s updating aamp->language from %s to %s mediaInfo[i].language %s\n", __FUNCTION__, aamp->language, this->mediaInfo[i].language ? this->mediaInfo[i].language : "en", mediaInfo[i].language);
 								aamp->UpdateAudioLanguageSelection((this->mediaInfo[i].language ? this->mediaInfo[i].language : "en"));
-								logprintf("GetPlaylistURI : language not found. Instead, select default of %s\n", aamp->language);
+								if(!this->mediaInfo[i].language)
+								{
+									logprintf("GetPlaylistURI : language not found. Instead, select default of %s\n", aamp->language);
+								}
 							}
 							playlistURI = this->mediaInfo[i].uri;
 							logprintf("GetPlaylistURI language found uri %s\n", playlistURI);
@@ -2639,6 +2645,8 @@ const char *StreamAbstractionAAMP_HLS::GetPlaylistURI(TrackType trackType, Strea
 	}
 	return playlistURI;
 }
+
+
 /***************************************************************************
 * @fn GetFormatFromFragmentExtension
 * @brief Function to get media format based on fragment extension
