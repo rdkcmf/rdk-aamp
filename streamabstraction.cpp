8ce02cc4a88c30ad6cb1915ff38395fa307afa36
Navaneeth H <navaneeth.h@tataelxsi.co.in>
2019-09-26 14:04:20 +0530

    DELIA-38371:[AAMP] Add ramp down logic for http error 502
    Reason for change: Added http error 502 into CheckForRampDownProfile function
    for ramp down as like in the case of 404. So that for 502 error rampdown will occur.
    
    Test Procedure: Tested on xg1v4.
    Risks: Low
    Signed-off-by: Navaneeth Haridas <navaneeth_haridas@comcast.com>
    
    Change-Id: Iff6cc5fb3bdf1a3b35ade7e9788397b0e39ea608
    (cherry picked from commit 7ec3283bdd3a9b572a733364237646cffec84f42)

--- streamabstraction.cpp
+++ streamabstraction.cpp
@@ -1433,7 +1433,7 @@ bool StreamAbstractionAAMP::CheckForRampDownProfile(long http_error)
 
 	if (!aamp->IsTSBSupported())
 	{
-		if (http_error == 404 || http_error == 500 || http_error == 503 || http_error == CURLE_PARTIAL_FILE)
+		if (http_error == 404 || http_error == 502 || http_error == 500 || http_error == 503 || http_error == CURLE_PARTIAL_FILE)
 		{
 			if (RampDownProfile(http_error))
 			{
