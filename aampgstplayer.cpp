fd8c44efe694925fa015dcabafcdbe1c9cf79a1a
Vinod Damodaran <vinod_damodaran@comcast.com>
2019-10-30 13:03:55 -0400

    DELIA-38923: Wrong playback position upon transition from FF/REW back to Play
    Reason for change : IdleCallback was not calling in the Apple builds,
                                       hence the play position was not updated.
    Test Procedure : Change the playback speed and check the play positions are proper.
    Risks : None
    
    Change-Id: Ief1f2843ba178cea705a6472700f2eddfff6d065
    Signed-off-by: Vinod Damodaran <vinod_damodaran@comcast.com>
    (cherry picked from commit 7a57c1357797756d4c33c904af497f8fc0bb4b58)
    (cherry picked from commit 64faac7c44ca6a3c70330b0baad3743f8f7861b8)

--- aampgstplayer.cpp
+++ aampgstplayer.cpp
@@ -869,7 +869,7 @@ static gboolean bus_message(GstBus * bus, GstMessage * msg, AAMPGstPlayer * _thi
 				_this->aamp->NotifyFirstFrameReceived();
 #endif
 
-#if defined(INTELCE)
+#if defined(INTELCE) || defined(__APPLE__)
 				//Note: Progress event should be sent after the decoderAvailable event only.
 				//BRCM platform sends progress event after AAMPGstPlayer_OnFirstVideoFrameCallback.
 				if (_this->privateContext->firstProgressCallbackIdleTaskId == 0)
