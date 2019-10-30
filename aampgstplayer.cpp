c2b77d216ae2a67887801be67339aac08e26e43c
jpadma058 <Jyotsna_Padmanabhan@comcast.com>
2019-10-30 06:14:05 +0000

    DELIA-38332 : Fix for no audio on linear channel or VOD resume with AAMP DASH IP on Raspberry Pi
    Reason for change : This change will invoke the changes done by custom patches for qtdemux,
                        which are required to fix no audio issue in RPI
    Test Procedure : Tested on RPI by Hao Li.
    
    Risks : NoneSigned-off-by : Jyotsna Padmanabhan <Jyotsna_Padmanabhan@comcast.com>
    
    Change-Id: If68cb56e59af96c4af0febe72d7cb33cc1eaef7b

--- aampgstplayer.cpp
+++ aampgstplayer.cpp
@@ -408,6 +408,11 @@ static GstCaps* GetGstCaps(StreamOutputFormat format)
 					"width", G_TYPE_INT, 1920,
 					"height", G_TYPE_INT, 1080,
 					NULL);
+#elif defined(RPI)
+			caps = gst_caps_new_simple ("video/x-h264",
+                                       "alignment", G_TYPE_STRING, "au",
+                                       "stream-format", G_TYPE_STRING, "avc",
+                                       NULL);
 #else
 			caps = gst_caps_new_simple ("video/x-h264", NULL, NULL);
 #endif
@@ -1661,13 +1666,13 @@ static void AAMPGstPlayer_SendPendingEvents(PrivateInstanceAAMP *aamp, AAMPGstPl
 	if (stream->format == FORMAT_ISO_BMFF)
 	{
 		gboolean enableOverride;
-#ifdef INTELCE
+#if (defined(INTELCE) || defined(RPI))
 		enableOverride = TRUE;
 #else
 		enableOverride = (privateContext->rate != AAMP_NORMAL_PLAY_RATE);
 #endif
 		GstStructure * eventStruct = gst_structure_new("aamp_override", "enable", G_TYPE_BOOLEAN, enableOverride, "rate", G_TYPE_FLOAT, (float)privateContext->rate, "aampplayer", G_TYPE_BOOLEAN, TRUE, NULL);
-#ifdef INTELCE
+#if (defined(INTELCE) || defined(RPI))
 		if ((privateContext->rate == AAMP_NORMAL_PLAY_RATE))
 		{
 			guint64 basePTS = aamp->GetFirstPTS() * GST_SECOND;
