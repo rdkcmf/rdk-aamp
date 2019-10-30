385f819f5f41204848e73eda9ad8c6d0f6bf5435
shripad bankar <Shripad_Bankar@cable.comcast.com>
2019-10-30 18:31:03 +0000

    DELIA-38949 : [AAMP] aamp.cfg is remnant in 3.14s3_PROD build.
    Reason for change: file name veriable initialzed if dev build else it will be empty.
                       hence aamp.cfg will work only in vbn build.
    
    Test Procedure:tested on xi5
    
    Change-Id: I72b1fcba98d1c86f020c180f8f1559d167678c76
    Risks:Low
    Signed-off-by: shripad bankar <Shripad_Bankar@cable.comcast.com>

--- main_aamp.cpp
+++ main_aamp.cpp
@@ -3431,7 +3431,7 @@ void PrivateInstanceAAMP::LazilyLoadConfigIfNeeded(void)
 		cfgPath += "/aamp.cfg";
 		FILE *f = fopen(cfgPath.c_str(), "rb");
 #else
-        cfgPath = "/opt/aamp.cfg";
+
 #ifdef AAMP_CPC // Comcast builds
         // AAMP_ENABLE_OPT_OVERRIDE is only added for PROD builds.
         const char *env_aamp_enable_opt = getenv("AAMP_ENABLE_OPT_OVERRIDE");
@@ -3439,10 +3439,9 @@ void PrivateInstanceAAMP::LazilyLoadConfigIfNeeded(void)
         const char *env_aamp_enable_opt = "true";
 #endif
 
-        FILE *f = NULL;
         if(env_aamp_enable_opt)
         {
-            f = fopen("/opt/aamp.cfg", "rb");
+            cfgPath = "/opt/aamp.cfg";
         }
 #endif
 		if (!cfgPath.empty())
@@ -3459,10 +3458,10 @@ void PrivateInstanceAAMP::LazilyLoadConfigIfNeeded(void)
 				}
 				f.close();
 			}
-		}
-		else
-		{
-			logprintf("Failed to open aamp.cfg\n");
+			else
+			{
+				logprintf("Failed to open aamp.cfg\n");
+			}
 		}
 
 		const char *env_aamp_force_aac = getenv("AAMP_FORCE_AAC");
