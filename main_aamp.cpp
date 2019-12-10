1f1c9b62a86e80a204cb3a3725d0dbe69e6dd397
shripad bankar <Shripad_Bankar@cable.comcast.com>
2019-12-10 22:48:34 +0000

    RDK-26546 : testing tag
    Reason for change: testing tag in topic branch
    
    Test Procedure:
    
    Change-Id: Ieb484978aeddac1adb17c9b44f111ade4fd81b8d
    Risks:Low
    Signed-off-by: shripad bankar <Shripad_Bankar@cable.comcast.com>

--- main_aamp.cpp
+++ main_aamp.cpp
@@ -4254,7 +4254,7 @@ void PrivateInstanceAAMP::Tune(const char *mainManifestUrl, const char *contentT
 	mIsFirstRequestToFOG = (mIsLocalPlayback == true);
 	if(mManifestUrl.length() < MAX_URL_LOG_SIZE)
 	{
-		logprintf("aamp_tune: attempt: %d format: %s URL: %s", mTuneAttempts, mIsDash?"DASH":"HLS", mManifestUrl.c_str());
+		logprintf("aamp_tune: TAG fr_test_fed_1 attempt: %d format: %s URL: %s", mTuneAttempts, mIsDash?"DASH":"HLS", mManifestUrl.c_str());
 	}
 	else
 	{
