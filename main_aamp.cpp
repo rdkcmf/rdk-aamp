49887fd903882ca5a39e9dcae9d555e2aa3b7c61
shripad bankar <Shripad_Bankar@cable.comcast.com>
2019-12-16 21:09:53 +0000

    RDK-26546 : aamp version logging changes for tag based release
    Reason for change:aamp version logging changes for tag based release
    
    Test Procedure:tested on xi5
    
    Change-Id: Ic34b5680af83e6067ca092d30153b10f413e2c9e
    Risks:Low
    Signed-off-by: shripad bankar <Shripad_Bankar@cable.comcast.com>
    
    RDK-26546 DELIA-40070 : aamp version logging changes for tag based release
    
    Reason for change: moved out #defines out of IARM block, so that it compiles on rasberry pi
    
    Test Procedure: tested on pi
    
    Change-Id: I4783014d36894e5df48d44f548beb0835942a70c
    Risks:Low
    Signed-off-by: shripad bankar <Shripad_Bankar@cable.comcast.com>

--- main_aamp.cpp
+++ main_aamp.cpp
@@ -71,7 +71,6 @@ static const char* strAAMPPipeName = "/tmp/ipc_aamp";
 #include <hostIf_tr69ReqHandler.h>
 #include <sstream>
 
-
 /**
  * @brief
  * @param paramName
@@ -81,6 +80,9 @@ static const char* strAAMPPipeName = "/tmp/ipc_aamp";
 char * GetTR181AAMPConfig(const char * paramName, size_t & iConfigLen);
 #endif
 
+//Stringification of Macro :  use two levels of macros
+#define MACRO_TO_STRING(s) X_STR(s)
+#define X_STR(s) #s
 
 /**
  * @brief get a character for console
@@ -3378,6 +3380,10 @@ void PrivateInstanceAAMP::LazilyLoadConfigIfNeeded(void)
 	std::string cfgPath = "";
 	if (!gpGlobalConfig)
 	{
+#ifdef AAMP_BUILD_INFO
+		std::string tmpstr = MACRO_TO_STRING(AAMP_BUILD_INFO);
+		logprintf(" AAMP_BUILD_INFO: %s",tmpstr.c_str());
+#endif
 		gpGlobalConfig = new GlobalConfigAAMP();
 #ifdef IARM_MGR 
         logprintf("LazilyLoadConfigIfNeeded calling  GetTR181AAMPConfig  ");
