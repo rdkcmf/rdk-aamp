86638ef5ce3ebddd93149d3979a589fda5f2455b
Vinish K B <Vinish_KorasseryBalakrishnan@comcast.com>
2020-03-18 09:20:13 +0000

    XRE-14727: Signature heapDecreaseKey App WPEWebProcess
    Reason for change: Send tuned event as synchronous if
    invoked from GMainLoop and as asynchronous if otherwise
    Test Procedure: Tested with arris xi6
    Risks: Low
    Signed-off-by: Vinish K B <Vinish_KorasseryBalakrishnan@comcast.com>
    
    Change-Id: I27b7489e2b06d5c9067ddc5df7429b441a8019b9

--- priv_aamp.h
+++ priv_aamp.h
@@ -536,7 +536,6 @@ public:
 	long defaultBitrate;        /**< Default bitrate*/
 	long defaultBitrate4K;      /**< Default 4K bitrate*/
 	bool bEnableABR;            /**< Enable/Disable adaptive bitrate logic*/
-	bool bAsync_ontuned;         /**< if true, AAMP_EVENT_TUNED will be sent Asynchronously otherwise will be sent synchronously */
 	bool SAP;                   /**< Enable/Disable Secondary Audio Program*/
 	bool noFog;                 /**< Disable FOG*/
 	int mapMPD;                 /**< Mapping of HLS to MPD: 0=Disable, 1=Rename m3u8 to mpd, 2=COAM mapping, 3='*-nat-*.comcast.net/' to 'ctv-nat-slivel4lb-vip.cmc.co.ndcwest.comcast.net/'*/
@@ -667,7 +666,6 @@ public:
 		,mSubtitleLanguage()
 		, enableClientDai(false), playAdFromCDN(false)
 		,mEnableVideoEndEvent(true)
-		,bAsync_ontuned(false)
 		,bReportVideoPTS(false)
 		,discontinuityTimeout(DEFAULT_DISCONTINUITY_TIMEOUT)
 		,mEnableRectPropertyCfg(eUndefinedState)
@@ -2530,9 +2528,10 @@ public:
 	/**
 	 *   @brief Send tuned event
 	 *
+	 *   @param[in] isSynchronous - send event synchronously or not
 	 *   @return success or failure
 	 */
-	bool SendTunedEvent();
+	bool SendTunedEvent(bool isSynchronous = true);
 
 	/**
 	 *   @brief Send VideoEndEvent
@@ -3174,27 +3173,28 @@ public:
 	 *   @return void
 	 */
 	void SetParallelPlaylistDL(bool bValue);
+
 	/**
 	 *   @brief Set async tune configuration
-	 *   @param[in] bValue - true if async tune enabled
 	 *
-	*   @return void
-	*/
+	 *   @param[in] bValue - true if async tune enabled
+	 *   @return void
+	 */
 	void SetAsyncTuneConfig(bool bValue);
 
 	/**
 	 *   @brief Get async tune configuration
 	 *
 	 *   @return bool - true if async tune enabled
-	*/
+	 */
 	bool GetAsyncTuneConfig();
 
 	/**
-	 *	 @brief Set Westeros sink Configuration
-	 *	 @param[in] bValue - true if westeros sink enabled
+	 *   @brief Set Westeros sink Configuration
 	 *
-	*	@return void
-	*/
+	 *   @param[in] bValue - true if westeros sink enabled
+	 *   @return void
+	 */
 	void SetWesterosSinkConfig(bool bValue);
 
 	/**
