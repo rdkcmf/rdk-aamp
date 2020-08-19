c7d33389c62485df701f85e20c545c826f00d247
deepa_jacob <deepa_jacob@cable.comcast.com>
2020-08-19 12:22:20 +0000

    RDK-29431 : Plugin calls added
    Reason for change: Added MediaPlayer and Hdmiin plugin calls
    and event handler using direct jsonrpc method.
    Test Procedure: Pending testing
    Risks: Low
    Signed-off-by:deepa_jacob@comcast.com
    
    Change-Id: I2a09fa28d04bf5c779bd98a09217d4c4d1b538b0
    
    AMLOGIC-588 : [CAD1.0][ATSC] scaling/placement incorrect when video not placed fullscreen
    
    Reason for change: MediaPlayer -> setVideoRectangle requires Screen width and height
                       Screen size is retrived from RdkShell plugin and passed to mediplayer
                       along with expected x,y,w,h of video window
    
    Test Procedure: atsc_test html app used to test scalling of videowindow from ATSC playback.
    
    Risks: Low
    Signed-off-by: shripad bankar <Shripad_Bankar@cable.comcast.com>
    Change-Id: Ia1a6c0ca354fa313c2faf07dd8bc0e13eea684c5
    
    AMLOGIC-588 : Separate plugin activation
    
    Reason for change: Seperate plugin activation
    from remote object creation.
    Test Procedure: Test OTA and hdmiin APIs
    Risks: Low
    Signed-off-by:deepa_jacob@comcast.com
    
    Change-Id: If3255a67c32fccbf018f274e4353142f275611fd
    
    RDK-29431 : Variable init fixes
    
    Reason for change: Added variable initialization
    fixes.
    Test Procedure: Test OTA video and events
    Risks: Low
    Signed-off-by:deepa_jacob@comcast.com
    
    Change-Id: Idd136733ef8d2129e389c844641369c82150f3b5
    
    RDK-29431 : Event handler modified for OTA
    
    Reason for change: Event handler modified for OTA.
    PREPARED event sequence changed for OTA.
    Test Procedure: Test OTA video events
    Risks: Low
    Signed-off-by:deepa_jacob@comcast.com
    
    Change-Id: Ia89b5dce089223defa4c137142564cbbc4636583

--- priv_aamp.cpp
+++ priv_aamp.cpp
@@ -4915,8 +4915,9 @@ void PrivateInstanceAAMP::TuneHelper(TuneType tuneType, bool seekWhilePaused)
 	{
 		PrivAAMPState state;
 		GetState(state);
-		if(state != eSTATE_ERROR)
+		if((state != eSTATE_ERROR) && (mMediaFormat != eMEDIAFORMAT_OTA))
 		{
+			/*For OTA this event will be generated from StreamAbstractionAAMP_OTA*/
 			SetState(eSTATE_PREPARED);
 		}
 	}
