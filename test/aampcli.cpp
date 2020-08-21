d38a9313c106131ec93fd5a8f0836146c8f7afb2
jkumar <Jai_Kumar@cable.comcast.com>
2020-08-21 20:36:54 +0000

    DELIA-38121: Refactor AAMPEvent
    Reason for change: compilation issue observed in OSX. c_str missing for
    string printf
    Test Procedure: Test using aampcli to read media metadata
    Risks: Low
    
    Signed-off-by: jkumar <Jai_Kumar@cable.comcast.com>
    Change-Id: I37999fdf638a71744df839ab0a8e1565800834eb

--- test/aampcli.cpp
+++ test/aampcli.cpp
@@ -428,7 +428,7 @@ public:
 				logprintf("AAMP_EVENT_MEDIA_METADATA\n" );
 				for (int i = 0; i < langCount; i++)
 				{
-					logprintf("language: %s\n", languages[i]);
+					logprintf("language: %s\n", languages[i].c_str());
 				}
 				break;
 			}
