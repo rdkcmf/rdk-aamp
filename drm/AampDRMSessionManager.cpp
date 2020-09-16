1d533c8927a1cac5d19984c36b5c7f9037729621
jkumar <Jai_Kumar@cable.comcast.com>
2020-09-16 04:11:39 +0000

    DELIA-45866: [AAMP][DASH] Switching between authorized and unauthorized channel throws "AAMP: Unknown Failure" error
    Reason for change: Multiple createDrmSession calls are received , per streamtype. In first call ,Authorization failure
    				identified and marked the keyFailed flag ,but cachedKeyIDs data is not removed . During next getDrmSession
    				call , new sessionSlot and data get assigned . Due to this sessionSlot is not cleared and get reused and ends
    				in failure with wrong state "key was never ready". Fix added to clear cachedKeyIDs and avoid same for multiple
    				createDrmSession call.
    
    Test Procedure: Channel tune between two unauthorized channels back to back and confirm the Sending error log.
    Risks: Low
    
    Signed-off-by: jkumar <Jai_Kumar@cable.comcast.com>
    Change-Id: I622759ea2884ea4038f89cc753d1e4ab74a3d931

--- drm/AampDRMSessionManager.cpp
+++ drm/AampDRMSessionManager.cpp
@@ -202,9 +202,12 @@ void AampDRMSessionManager::clearFailedKeyIds()
 	pthread_mutex_lock(&cachedKeyMutex);
 	for(int i = 0 ; i < gpGlobalConfig->dash_MaxDRMSessions; i++)
 	{
-		if((!cachedKeyIDs[i].data.empty()) && cachedKeyIDs[i].isFailedKeyId)
+		if(cachedKeyIDs[i].isFailedKeyId)
 		{
-			cachedKeyIDs[i].data.clear();
+			if(!cachedKeyIDs[i].data.empty())
+			{
+				cachedKeyIDs[i].data.clear();
+			}
 			cachedKeyIDs[i].isFailedKeyId = false;
 			cachedKeyIDs[i].creationTime = 0;
 		}
@@ -838,7 +841,7 @@ AampDrmSession* AampDRMSessionManager::createDrmSession(std::shared_ptr<AampDrmH
 
 	int selectedSlot = INVALID_SESSION_SLOT;
 
-	AAMPLOG_INFO("%s:%d keySystem is %s", __FUNCTION__, __LINE__, drmHelper->ocdmSystemId().c_str());
+	AAMPLOG_INFO("%s:%d StreamType :%d keySystem is %s", __FUNCTION__, __LINE__,streamType, drmHelper->ocdmSystemId().c_str());
 
 	/**
 	 * Create drm session without primaryKeyId markup OR retrieve old DRM session.
@@ -953,6 +956,16 @@ KeyState AampDRMSessionManager::getDrmSession(std::shared_ptr<AampDrmHelper> drm
 			}
 			logprintf("%s:%d  Selected slot %d for keyId %s",__FUNCTION__, __LINE__, sessionSlot, keyIdDebugStr.c_str());
 		}
+		else
+		{
+			// Already same session Slot is marked failed , not to proceed again .
+			if(cachedKeyIDs[sessionSlot].isFailedKeyId)
+			{
+				logprintf("%s:%d Found FailedKeyId at sesssionSlot :%d, return key error",__FUNCTION__, __LINE__,sessionSlot);
+				return KEY_ERROR;
+			}
+		}
+		
 
 		if (!isCachedKeyId)
 		{
@@ -1247,6 +1260,7 @@ KeyState AampDRMSessionManager::handleLicenseResponse(std::shared_ptr<AampDrmHel
 				{
 					eventHandle->setFailure(AAMP_TUNE_AUTHORISATION_FAILURE);
 				}
+				AampMutexHold sessionMutex(drmSessionContexts[sessionSlot].sessionMutex);
 				AAMPLOG_WARN("%s:%d deleting existing DRM session for %s, Authorisation failed", __FUNCTION__, __LINE__, drmSessionContexts[sessionSlot].drmSession->getKeySystem().c_str());
 				delete drmSessionContexts[sessionSlot].drmSession;
 				drmSessionContexts[sessionSlot].drmSession = nullptr;
@@ -1260,6 +1274,7 @@ KeyState AampDRMSessionManager::handleLicenseResponse(std::shared_ptr<AampDrmHel
 				eventHandle->setFailure(AAMP_TUNE_LICENCE_REQUEST_FAILED);
 				eventHandle->setResponseCode(httpResponseCode);
 			}
+			AampMutexHold keymutex(cachedKeyMutex);
 			cachedKeyIDs[sessionSlot].isFailedKeyId = true;
 
 			return KEY_ERROR;
