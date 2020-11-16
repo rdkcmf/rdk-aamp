efb907914a8a0152998c543f5524e2173385fd48
jkumar <Jai_Kumar@cable.comcast.com>
2020-11-16 21:08:14 +0000

    DELIA-47236 : [DAI] [SAP] VOD Entertainment plays in English audio even User prefferred language is Spanish
    Reason for change: Across period change , language from audio adaptation is set and modifies user prefferred language .
    This causes the english language from Ad period to select english content from Content Period.
    
    Test Procedure: Play with DASH VOD(DAI) with SAP audio .Do trickplay across Ads.
    Risks: Low
    
    
    Signed-off-by: jkumar <Jai_Kumar@cable.comcast.com>
    Change-Id: I1861e6ba85028861b5333cc5fa5e6a02478c5c73
    (cherry picked from commit baeaaa41d2b82287be323d684750c4e9490116d4)
    (cherry picked from commit 8033d5f22791e9e5549b459706d8050052bc7c7b)

--- fragmentcollector_mpd.cpp
+++ fragmentcollector_mpd.cpp
@@ -5204,7 +5204,7 @@ void PrivateStreamAbstractionMPD::StreamSelection( bool newTune, bool forceSpeed
 	if( audioAdaptationSet )
 	{
 		std::string lang = GetLanguageForAdaptationSet(audioAdaptationSet);
-		aamp->UpdateAudioLanguageSelection( lang.c_str() );
+		aamp->UpdateAudioLanguageSelection( lang.c_str() , false );
 		if(desiredRepIdx != -1 )
 		{
 			audioRepresentationIndex = desiredRepIdx;
