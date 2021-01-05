dc9ce195fb1bd9998bf38ecd6638aa0aab944ee8
Akhil Babu <akhil.babu@sky.uk>
2021-01-05 18:22:36 +0000

    AMLOGIC-1085 / LLAMA-613 : Audio missing for live and VOD
    Reason for change: Add compile time flag for AMLOGIC SOC
    AMLOGIC audio sink doesn't support aac audio
    That needs to be decoded to raw
    
    Test Procedure: Play HLS contents from EPG
    Risks: Low
    
    
    Signed-off-by: Akhil Babu <akhil.babu@sky.uk>
    Change-Id: I120b7b1b1e3cb00477c313b3e4a068af82672624
    (cherry picked from commit 503020f9e97aeb4f6933926879eed5e995f7e4ad)

--- aampgstplayer.cpp
+++ aampgstplayer.cpp
@@ -1847,7 +1847,7 @@ static int AAMPGstPlayer_SetupStream(AAMPGstPlayer *_this, MediaType streamId)
 		gint flags;
 		g_object_get(stream->sinkbin, "flags", &flags, NULL);
 		logprintf("playbin flags1: 0x%x", flags); // 0x617 on settop
-#if (defined(__APPLE__) || defined(UBUNTU))
+#if (defined(__APPLE__) || defined(NO_NATIVE_AV)) 
 		flags = GST_PLAY_FLAG_VIDEO | GST_PLAY_FLAG_AUDIO;
 #elif defined (REALTEKCE)
 		flags = GST_PLAY_FLAG_VIDEO | GST_PLAY_FLAG_AUDIO | GST_PLAY_FLAG_NATIVE_AUDIO | GST_PLAY_FLAG_NATIVE_VIDEO | GST_PLAY_FLAG_SOFT_VOLUME;
