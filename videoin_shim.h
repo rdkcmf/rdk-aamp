8554d3c27945ece8f8fa1967a1d9b31964e88f86
shripad bankar <Shripad_Bankar@cable.comcast.com>
2021-01-12 06:04:09 +0000

    RDKTV-975 : Composite Input SHIM support
    Reason for change: added support for composite input for UVE
    Test Procedure: tested with hisense TV
    Risks: Low
    
    Signed-off-by: shripad bankar <Shripad_Bankar@cable.comcast.com>
    Change-Id: I591de4a2d872e80665981b5d11e6130b979fd420

---
+++ videoin_shim.h
@@ -0,0 +1,85 @@
+/*
+ * If not stated otherwise in this file or this component's license file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2021 RDK Management
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+/**
+ * @file videoin_shim.h
+ * @brief shim for dispatching UVE Video input playback
+ */
+
+#ifndef VIDEOIN_SHIM_H_
+#define VIDEOIN_SHIM_H_
+
+#include "StreamAbstractionAAMP.h"
+#include <string>
+#include <stdint.h>
+#ifdef USE_CPP_THUNDER_PLUGIN_ACCESS
+#include <core/core.h>
+#include "ThunderAccess.h"
+#endif
+using namespace std;
+
+/**
+ * @class StreamAbstractionAAMP_VIDEOIN
+ * @brief Fragment collector for MPEG DASH
+ */
+class StreamAbstractionAAMP_VIDEOIN : public StreamAbstractionAAMP
+{
+public:
+    StreamAbstractionAAMP_VIDEOIN(const std::string name, const std::string callSign, class PrivateInstanceAAMP *aamp,double seekpos, float rate);
+    ~StreamAbstractionAAMP_VIDEOIN();
+    StreamAbstractionAAMP_VIDEOIN(const StreamAbstractionAAMP_VIDEOIN&) = delete;
+    StreamAbstractionAAMP_VIDEOIN& operator=(const StreamAbstractionAAMP_VIDEOIN&) = delete;
+    void DumpProfiles(void) override;
+    void Start() override;
+    void Stop(bool clearChannelData) override;
+    void SetVideoRectangle(int x, int y, int w, int h) override;
+    AAMPStatusType Init(TuneType tuneType) override;
+    void GetStreamFormat(StreamOutputFormat &primaryOutputFormat, StreamOutputFormat &audioOutputFormat/*, StreamOutputFormat &auxAudioOutputFormat*/) override;
+    double GetStreamPosition() override;
+    MediaTrack* GetMediaTrack(TrackType type) override;
+    double GetFirstPTS() override;
+    double GetBufferedDuration() override;
+    bool IsInitialCachingSupported() override;
+    int GetBWIndex(long bitrate) override;
+    std::vector<long> GetVideoBitrates(void) override;
+    std::vector<long> GetAudioBitrates(void) override;
+    long GetMaxBitrate(void) override;
+    void StopInjection(void) override;
+    void StartInjection(void) override;
+    void SeekPosUpdate(double) { };
+	void NotifyFirstVideoPTS(unsigned long long pts) { };
+protected:
+    StreamInfo* GetStreamInfo(int idx) override;
+    void StartHelper(int port, const std::string & methodName);
+    void StopHelper(const std::string & methodName) ;
+private:
+#ifdef USE_CPP_THUNDER_PLUGIN_ACCESS
+    ThunderAccessAAMP thunderAccessObj;
+    ThunderAccessAAMP thunderRDKShellObj;
+#endif
+    bool GetScreenResolution(int & screenWidth, int & screenHeight);
+	int videoInputPort;
+	std::string mName; // Used for logging
+};
+
+#endif // VIDEOIN_SHIM_H_
+/**
+ * @}
+ */
+
