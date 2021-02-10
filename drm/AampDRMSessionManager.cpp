9c66a1f253c274c8ba478df7a03f809c41bc1f2d
dathia184 <dhanapal_athiappagounder@comcast.com>
2021-02-10 12:52:18 +0000

    DELIA-48647: Peacock Playback failure with DRM License Request Failed Secclient  -1 error code
    Reason for change: In problematic case after peacock aamp_tune DRM License Request, aamp_Seek is called before DRM License response and this cause DRM license abort with assigning curl response code as -1 in getLicense()  API. Now added fix to assign response code as 42 - curl abort into http_code before return from getLicense()
    
    Test Procedure: To tested with Peacock content resume playback and normal linear channel change
    Risks: low
    
    Change-Id: I74f68ae48b0e8ee7dd2b846aeb4fb380befc399e
    Signed-off-by: Dhanapal A <dhanapal_athiappagounder@comcast.com>
    (cherry picked from commit 1b263d8d5c71a32dc58dcb6e2e5715939dcd6107)
    (cherry picked from commit db87e281ba29e5f1908da65066044a4fe694d938)

--- drm/AampDRMSessionManager.cpp
+++ drm/AampDRMSessionManager.cpp
@@ -792,6 +792,13 @@ DrmData * AampDRMSessionManager::getLicense(AampLicenseRequest &licenseRequest,
 			break;
 	}
 
+	if(*httpCode == -1)
+	{
+		logprintf("%s:%d Updating Curl Abort Response Code", __FUNCTION__, __LINE__);
+		// Update httpCode as 42-curl abort, so that DRM error event will not be sent by upper layer
+		*httpCode = CURLE_ABORTED_BY_CALLBACK;
+	}
+
 	if(requestFailed && keyInfo != NULL)
 	{
 		delete keyInfo;
@@ -1215,6 +1222,7 @@ KeyState AampDRMSessionManager::acquireLicense(std::shared_ptr<AampDrmHelper> dr
 			if(licenseRequestAbort)
 			{
 				AAMPLOG_ERR("%s:%d Error!! License request was aborted. Resetting session slot %d", __FUNCTION__, __LINE__, sessionSlot);
+				eventHandle->setResponseCode(CURLE_ABORTED_BY_CALLBACK);
 				return KEY_ERROR;
 			}
 
