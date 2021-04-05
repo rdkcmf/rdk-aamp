aa51a90eb4398f7cfed6cd63539b4e6e2242b757
Vinish K B <Vinish_KorasseryBalakrishnan@comcast.com>
2021-04-05 13:18:11 +0530

    RDK-32080: Async tune feature
    Reason for change: Reorder events to sent TimedMetadata or
    BulkTimedMetadata before MediaMetadata
    Test Procedure: Test cases available in ticket
    Risks: Low
    
    DEPS_BRANCH=rdk/components/generic/gst-plugins-rdk-aamp:dev_sprint
    DEPS_BRANCH=rdk/components/generic/aampabr:dev_sprint
    DEPS_BRANCH=rdk/components/generic/aampmetrics:dev_sprint
    
    Signed-off-by: Vinish K B <Vinish_KorasseryBalakrishnan@comcast.com>
    Change-Id: I4bb20e5fffe6d9aaa60c9993e899f0a4b9017080

--- fragmentcollector_hls.cpp
+++ fragmentcollector_hls.cpp
@@ -4422,8 +4422,6 @@ AAMPStatusType StreamAbstractionAAMP_HLS::Init(TuneType tuneType)
 			return eAAMPSTATUS_PLAYLIST_AUDIO_DOWNLOAD_ERROR;
 		}
 
-		bool bSetStatePreparing = false;
-
 		if (rate != AAMP_NORMAL_PLAY_RATE)
 		{
 			trickplayMode = true;
@@ -4501,18 +4499,6 @@ AAMPStatusType StreamAbstractionAAMP_HLS::Init(TuneType tuneType)
 						// Send bulk report
 						aamp->ReportBulkTimedMetadata();
 					}
-				}
-
-				if (newTune && needMetadata)
-				{
-					needMetadata = false;
-					std::vector<long> bitrateList;
-					bitrateList = GetVideoBitrates();
-					aamp->mIsIframeTrackPresent = mIframeAvailable;
-					aamp->SendMediaMetadataEvent((ts->mDuration * 1000.0), mLangList, bitrateList, hasDrm, aamp->mIsIframeTrackPresent);
-					// Delay "preparing" state until all tracks have been processed.
-					// JS Player assumes all onTimedMetadata event fire before "preparing" state.
-					bSetStatePreparing = true;
 				}
 
 				if (iTrack == eMEDIATYPE_VIDEO)
@@ -4819,8 +4805,29 @@ AAMPStatusType StreamAbstractionAAMP_HLS::Init(TuneType tuneType)
 			return eAAMPSTATUS_MANIFEST_CONTENT_ERROR;
 		}
 
-		if (bSetStatePreparing)
+		if (newTune && needMetadata)
+		{
+			needMetadata = false;
+			std::vector<long> bitrateList = GetVideoBitrates();
+			aamp->mIsIframeTrackPresent = mIframeAvailable;
+			double duration = 0;
+			if (trackState[eTRACK_VIDEO]->enabled && trackState[eTRACK_VIDEO]->mDuration != 0)
+			{
+				duration = trackState[eTRACK_VIDEO]->mDuration;
+			}
+			else if (trackState[eTRACK_AUDIO]->enabled && trackState[eTRACK_AUDIO]->mDuration != 0)
+			{
+				duration = trackState[eTRACK_AUDIO]->mDuration;
+			}
+			else
+			{
+				AAMPLOG_WARN("StreamAbstractionAAMP_HLS::%s:%d No valid duration found in audio and video, sending duration as 0", __FUNCTION__, __LINE__);
+			}
+			aamp->SendMediaMetadataEvent((duration * 1000.0), mLangList, bitrateList, hasDrm, aamp->mIsIframeTrackPresent);
+			// Delay "preparing" state until all tracks have been processed.
+			// JS Player assumes all onTimedMetadata event fire before "preparing" state.
 			aamp->SetState(eSTATE_PREPARING);
+		}
 
 		//Currently un-used playlist indexed event, might save some JS overhead
 		if (!gpGlobalConfig->disablePlaylistIndexEvent)
