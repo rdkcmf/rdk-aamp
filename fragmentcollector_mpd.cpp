80465a3625b1f47e994d22b789346b290074e33a
Nandakishor Udiyannur Mana <Nandakishor_UdiyannurMana@comcast.com>
2021-04-09 18:34:58 +0530

    DELIA-49707 :[AAMP][PEACOCK] DASH MT Harmonic Stream buffering issue .
    Reason for change: Fixing period duration calculation issue for larger
    timescales.
    Risks: Low
    Test Procedure: Test with STBs.
    Signed-off-by: Nandakishor Udiyannur<Nandakishor_udiyannurmana@comcast.com>
    Change-Id: I6fac3fb82d7d51b4c15c7b832a6435b89c7c1484

--- fragmentcollector_mpd.cpp
+++ fragmentcollector_mpd.cpp
@@ -2513,7 +2513,7 @@ double PrivateStreamAbstractionMPD::SkipFragments( MediaStreamContext *pMediaStr
 				bool isFogTsb = !(rawAttributes.find("customlist") == rawAttributes.end());
 				if(!isFogTsb)
 				{
-					segmentDuration = segmentList->GetDuration() / timescale;
+					segmentDuration = ComputeFragmentDuration( segmentList->GetDuration() , timescale);
 				}
 				else if(pMediaStreamContext->type == eTRACK_AUDIO)
 				{
@@ -3176,7 +3176,7 @@ uint64_t aamp_GetPeriodNewContentDuration(IPeriod * period, uint64_t &curEndNumb
 				{
 					ITimeline *timeline = timelines.at(timeLineIndex);
 					uint32_t segmentCount = timeline->GetRepeatCount() + 1;
-					uint32_t timelineDurationMs = timeline->GetDuration() * 1000 / timeScale;
+					uint32_t timelineDurationMs = ComputeFragmentDuration(timeline->GetDuration(),timeScale) * 1000;
 					for(int i=0;i<segmentCount;i++)
 					{
 						if(startNumber > curEndNumber)
@@ -3445,7 +3445,7 @@ uint64_t aamp_GetPeriodDuration(dash::mpd::IMPD *mpd, int periodIndex, uint64_t 
 						{
 							ITimeline *timeline = timelines.at(timeLineIndex);
 							uint32_t repeatCount = timeline->GetRepeatCount();
-							uint32_t timelineDurationMs = timeline->GetDuration() * 1000 / timeScale;
+							uint32_t timelineDurationMs = ComputeFragmentDuration(timeline->GetDuration(),timeScale) * 1000;
 							durationMs += ((repeatCount + 1) * timelineDurationMs);
 							AAMPLOG_TRACE("%s timeLineIndex[%d] size [%lu] updated durationMs[%" PRIu64 "]", __FUNCTION__, timeLineIndex, timelines.size(), durationMs);
 							timeLineIndex++;
@@ -3551,7 +3551,7 @@ uint64_t aamp_GetPeriodDuration(dash::mpd::IMPD *mpd, int periodIndex, uint64_t 
 					if (segmentList)
 					{
 						const std::vector<ISegmentURL*> segmentURLs = segmentList->GetSegmentURLs();
-						durationMs += (double) segmentList->GetDuration() * 1000 / segmentList->GetTimescale();
+						durationMs += ComputeFragmentDuration( segmentList->GetDuration(), segmentList->GetTimescale()) * 1000;
 					}
 					else
 					{
@@ -4139,7 +4139,7 @@ uint64_t aamp_GetDurationFromRepresentation(dash::mpd::IMPD *mpd)
 						{
 							ITimeline *timeline = timelines.at(timeLineIndex);
 							uint32_t repeatCount = timeline->GetRepeatCount();
-							uint32_t timelineDurationMs = timeline->GetDuration() * 1000 / timeScale;
+							uint32_t timelineDurationMs = ComputeFragmentDuration( timeline->GetDuration(), timeScale) * 1000;
 							durationMs += ((repeatCount + 1) * timelineDurationMs);
 							traceprintf("%s period[%d] timeLineIndex[%d] size [%lu] updated durationMs[%" PRIu64 "]", __FUNCTION__, iPeriod, timeLineIndex, timelines.size(), durationMs);
 							timeLineIndex++;
@@ -4156,7 +4156,7 @@ uint64_t aamp_GetDurationFromRepresentation(dash::mpd::IMPD *mpd)
                                         if (segmentList)
                                         {
                                                 const std::vector<ISegmentURL*> segmentURLs = segmentList->GetSegmentURLs();
-                                                durationMs += (double) segmentList->GetDuration() * 1000 / segmentList->GetTimescale();
+						durationMs += ComputeFragmentDuration(segmentList->GetDuration(), segmentList->GetTimescale()) * 1000;	
                                         }
                                         else
                                         {
@@ -6121,7 +6121,7 @@ double PrivateStreamAbstractionMPD::GetCulledSeconds()
 				if(firstSegTempate)
 				{
 					newStartSegment = (double)firstSegTempate->GetStartNumber();
-					double fragmentDuration = ((double)segmentTemplates.GetDuration()) / segmentTemplates.GetTimescale();
+					double fragmentDuration = ComputeFragmentDuration( segmentTemplates.GetDuration(), segmentTemplates.GetTimescale());
 					if (newStartSegment && mPrevStartTimeSeconds)
 					{
 						culled = (newStartSegment - mPrevStartTimeSeconds) * fragmentDuration;
@@ -8032,8 +8032,7 @@ bool PrivateStreamAbstractionMPD::isAdbreakStart(IPeriod *period, uint32_t &dura
 								{
 									timeScale = eventStream->GetTimescale();
 								}
-								//first multiply then divide to avoid magnifying rounding off issues.
-								duration = ((uint64_t)event->GetDuration()*1000)/timeScale; //milliseconds
+								duration = ComputeFragmentDuration(event->GetDuration(), timeScale) * 1000; //milliseconds
 								scte35 = signalChild->GetText();
 								if(scte35.length())
 								{
