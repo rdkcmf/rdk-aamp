21c272ef69586db84efe16e48a41e61acf8e96cd
sivasubramanian <sivasubramanian.patchaiperumal@ltts.com>
2021-04-22 02:00:19 +0000

    XIONE-2210: [SKY][NGDEV-174560] Convia not showing buffering at tune time or mid-playback with Sky AS / AAMP integration
    Reason for change: Enable Prebuffering and underflow handling as queued_frames is now updating
    Test Procedure: Test prebuffering & underflow testcases
    Risks: Realtek specific
    
    Signed-off-by: sivasubramanian <sivasubramanian.patchaiperumal@ltts.com>
    Change-Id: I8f8255f4aed4fbbec4f1cf1e9b412855193343cb
    (cherry picked from commit a31435f42d95110606d566f1fb95981f378d075b)

--- GlobalConfigAAMP.cpp
+++ GlobalConfigAAMP.cpp
@@ -83,7 +83,7 @@ GlobalConfigAAMP::GlobalConfigAAMP():
 	, mEnableSeekableRange(eUndefinedState)
 	, mPersistBitRateOverSeek(eUndefinedState)
 	, licenseCaching(eUndefinedState)
-	, bDisableUnderflow(true)
+	, bDisableUnderflow(false)
 {
 	//XRE sends onStreamPlaying while receiving onTuned event.
 	//onVideoInfo depends on the metrics received from pipe.
