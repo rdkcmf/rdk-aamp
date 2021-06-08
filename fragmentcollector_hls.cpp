860ad0631b08d3c9eb951ac9f8179639149936d5
Madhavan Krishnan <madhavan_Krishnan@comcast.com>
2021-06-08 09:28:32 +0000

    DELIA-50771 [AAMP] tsprocessor poor handling of packets starting with ID3
    Reason for change: AC3 audio format is ES data, and no ts processing required.
    So avoided playcontext creation for AC3 format.
    
    Test Procedure: Test all the hls files having separate audio and video manifests.
    
    Risks: Low
    
    DEPS_BRANCH=rdk/components/generic/gst-plugins-rdk-aamp:dev_sprint
    DEPS_BRANCH=rdk/components/generic/aampabr:dev_sprint
    DEPS_BRANCH=rdk/components/generic/aampmetrics:dev_sprint
    
    Signed-off-by: Madhavan Krishnan <madhavan_Krishnan@comcast.com>
    Change-Id: I0f11569ea1a64a6a8b6aedcf4e06d43bf97daf6c

--- fragmentcollector_hls.cpp
+++ fragmentcollector_hls.cpp
@@ -3407,6 +3407,10 @@ static StreamOutputFormat GetFormatFromFragmentExtension(TrackState *trackState)
 				{
 					format = FORMAT_AUDIO_ES_AAC;
 				}
+				else if ( extension == ".ac3" )
+				{
+					format = FORMAT_AUDIO_ES_AC3;
+				}
 				else if ( extension == ".ec3" )
 				{
 					format = FORMAT_AUDIO_ES_EC3;
@@ -4445,6 +4449,14 @@ AAMPStatusType StreamAbstractionAAMP_HLS::Init(TuneType tuneType)
 				{
 					logprintf("StreamAbstractionAAMP_HLS::Init : Track[%s] - FORMAT_AUDIO_ES_AAC", ts->name);
 					ts->streamOutputFormat = FORMAT_AUDIO_ES_AAC;
+					continue;
+				}
+
+				// Elementary stream, we can skip playContext creation
+				if (FORMAT_AUDIO_ES_AC3 == format)
+				{
+					logprintf("StreamAbstractionAAMP_HLS::Init : Track[%s] - FORMAT_AUDIO_ES_AC3", ts->name);
+					ts->streamOutputFormat = FORMAT_AUDIO_ES_AC3;
 					continue;
 				}
 
