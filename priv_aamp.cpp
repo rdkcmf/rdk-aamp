9c60d8ac09959b72c9e163d977863ba547b5119a
Paulpandian M <paulpandian_mariappan@cable.comcast.com>
2021-07-12 20:22:01 +0000

    DELIA-51241 [AAMP] [Peacock] Playback froze at the beginning of the AD, seeking after AD recovers the issue.
    Reason for change: No need to process discontinuity stall if already a discontinuity-process in-progress
    or the discontinuity handler spawned(%d)
    Test Procedure: Playback Peacock VOD/SLE, no freeze should happen during the time of Content to AD transition.
    Risks: Low
    
    DEPS_BRANCH=rdk/components/generic/gst-plugins-rdk-aamp:dev_sprint_21_1
    DEPS_BRANCH=rdk/components/generic/aampabr:dev_sprint_21_1
    DEPS_BRANCH=rdk/components/generic/aampmetrics:dev_sprint_21_1
    
    Signed-off-by: Paulpandian M <paulpandian_mariappan@cable.comcast.com>
    Change-Id: Iad05b9a294ab66c9672a100d03eda6bc168aeded
    (cherry picked from commit b52a22d45026f63f66388a6249a79561ac6385d9)
    (cherry picked from commit 4c1e1307fccff3ad6d11b009969c06b2bc5b2a3a)

--- priv_aamp.cpp
+++ priv_aamp.cpp
@@ -4876,6 +4876,18 @@ void PrivateInstanceAAMP::CheckForDiscontinuityStall(MediaType mediaType)
 	GETCONFIGVALUE_PRIV(eAAMPConfig_DiscontinuityTimeout,discontinuityTimeoutValue);
 	if(!(mStreamSink->CheckForPTSChangeWithTimeout(discontinuityTimeoutValue)))
 	{
+		pthread_mutex_lock(&mLock);
+
+		if (mDiscontinuityTuneOperationId != 0 || mDiscontinuityTuneOperationInProgress)
+		{
+			logprintf("PrivateInstanceAAMP::%s:%d: Ignored retune!! Discontinuity handler already spawned(%d) or inprogress(%d)",
+							__FUNCTION__, __LINE__, mDiscontinuityTuneOperationId, mDiscontinuityTuneOperationInProgress);
+			pthread_mutex_unlock(&mLock);
+			return;
+		}
+
+		pthread_mutex_unlock(&mLock);
+
 		AAMPLOG_INFO("%s:%d : No change in PTS for more than %ld ms, schedule retune!",__FUNCTION__, __LINE__, discontinuityTimeoutValue);
 		mProcessingDiscontinuity[eMEDIATYPE_VIDEO] = false;
 		mProcessingDiscontinuity[eMEDIATYPE_AUDIO] = false;
@@ -6191,9 +6203,9 @@ void PrivateInstanceAAMP::ScheduleRetune(PlaybackErrorType errorType, MediaType 
 		{
 			if (mDiscontinuityTuneOperationId != 0 || mDiscontinuityTuneOperationInProgress)
 			{
-				pthread_mutex_unlock(&mLock);
 				logprintf("PrivateInstanceAAMP::%s:%d: Discontinuity Tune handler already spawned(%d) or inprogress(%d)",
 					__FUNCTION__, __LINE__, mDiscontinuityTuneOperationId, mDiscontinuityTuneOperationInProgress);
+				pthread_mutex_unlock(&mLock);
 				return;
 			}
 			mDiscontinuityTuneOperationId = ScheduleAsyncTask(PrivateInstanceAAMP_ProcessDiscontinuity, (void *)this);
