244bfc93714bf2c01228e9df9eb3509790fdf382
Paulpandian M <paulpandian_mariappan@cable.comcast.com>
2021-08-10 05:26:39 +0000

    DELIA-51792 [AAMP] [Peacock] Duplicate progress events sent after seek for 2 secs to the application.
    Reason for change: Reverted hack added in DELIA-50869 and added logic in player to avoid sending duplicate position events for a sec.
    This patch needs to go along with DELIA-50920.
    Test Procedure: Playback Peacock DASH VOD try to seek around the AD places and see the progress event. No duplication should be there.
    Risks: Low
    Signed-off-by: Paulpandian M <paulpandian_mariappan@cable.comcast.com>
    Change-Id: I4c48dc503519e15bff1f85afeee966aa46eff3aa

--- main_aamp.cpp
+++ main_aamp.cpp
@@ -646,7 +646,6 @@ void PlayerInstanceAAMP::SetRate(int rate,int overshootcorrection)
 					aamp->mpStreamAbstractionAAMP->NotifyPlaybackPaused(false);
 					retValue = aamp->mStreamSink->Pause(false, false);
 					aamp->NotifyFirstBufferProcessed(); //required since buffers are already cached in paused state
-					aamp->ResetTrickStartUTCTime();
 				}
 				aamp->pipeline_paused = false;
 				aamp->ResumeDownloads();
--- priv_aamp.h
+++ priv_aamp.h
@@ -1860,13 +1860,6 @@ public:
 	void UpdateSubtitleTimestamp();
 
 	/**
-         *  @brief Reset trick start position
-         *
-         *  @return void
-         */
-        void ResetTrickStartUTCTime();
-
-	/**
 	 *   @brief Get stream type
 	 *
 	 *   @return Stream type
