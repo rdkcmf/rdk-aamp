8d072695039179ff471d588da5b8988083bad56c
sivasubramanian <sivasubramanian.patchaiperumal@ltts.com>
2021-09-20 13:04:57 -0400

    XIONE-6138 : Black Screen is observed after tune to UHD-linear channel
    Reason for change: Enable multiqueue for reducing enough-data emitting from aapsrc.
    Test Procedure: Verify on channel 1012
    Risks: Low
    
    DEPS_BRANCH=rdk/components/generic/gst-plugins-rdk-aamp:4.12_p1xione_us
    DEPS_BRANCH=rdk/components/generic/aampabr:4.12_p1xione_us
    DEPS_BRANCH=rdk/components/generic/aampmetrics:4.12_p1xione_us
    
    Revert "Revert "XIONE-6138 : Black Screen is observed after tune to UHD-linear channel""
    This reverts commit a1a58129028ba7e85350a9f33edb40749f9447ab.
    
    Change-Id: I6fb6e3208d648f0d46fefbd38ca331272a98918f

--- priv_aamp.cpp
+++ priv_aamp.cpp
@@ -7004,6 +7004,16 @@ int PrivateInstanceAAMP::getStreamType()
 	return type;
 }
 
+/**
+ * @brief Get Mediaformat type
+ *
+ * @returns eMEDIAFORMAT
+*/
+MediaFormat PrivateInstanceAAMP::GetMediaFormatTypeEnum() const
+{
+	return mMediaFormat;
+}
+
 #ifdef USE_SECCLIENT
 /**
  * @brief GetMoneyTraceString - Extracts / Generates MoneyTrace string
--- priv_aamp.h
+++ priv_aamp.h
@@ -1866,6 +1866,13 @@ public:
 	int getStreamType();
 
 	/**
+	 *   @brief Get Mediaformat types
+	 *
+	 *   @return eMEDIAFORMAT
+	 */
+	MediaFormat GetMediaFormatTypeEnum() const;
+
+	/**
 	 *   @brief Get stream type as printable format
 	 *
 	 *   @return Stream type as string
