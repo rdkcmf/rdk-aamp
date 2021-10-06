8f46a2844e63662eb015a0938411971c9cab17cc
jcheer699 <JasirMohammed_Cheeroth@comcast.com>
2021-10-06 12:27:51 -0400

    DELIA-52041 : Trickplay from ad to content results in 'missing Gstreamer plugin' error Reason for change : Added support for encrypted iframe tracks
    Test Procedure: Will update in the ticket
    
    Risks: low
    
    Signed-off-by: JasirMohammed_Cheeroth@comcast.com
    Change-Id: I3f1b67cdff88cbfa0ba32cfbe23051df3063c597
    (cherry picked from commit 9dc8c417c5459a3ded117dcd5044774f04d91050)
    (cherry picked from commit 67951f54668ed09b7a4b6739b1955c4d7d460ebb)

--- fragmentcollector_mpd.cpp
+++ fragmentcollector_mpd.cpp
@@ -5801,6 +5801,7 @@ void StreamAbstractionAAMP_MPD::StreamSelection( bool newTune, bool forceSpeedsC
 		int  selAdaptationSetIndex = -1;
 		int selRepresentationIndex = -1;
 		bool isIframeAdaptationAvailable = false;
+		bool encryptedIframeTrackPresent = false;
 		int videoRepresentationIdx;   //CID:118900 - selRepBandwidth variable locally declared but not reflected
 		for (unsigned iAdaptationSet = 0; iAdaptationSet < numAdaptationSets; iAdaptationSet++)
 		{
@@ -5929,6 +5930,11 @@ void StreamAbstractionAAMP_MPD::StreamSelection( bool newTune, bool forceSpeedsC
 						pMediaStreamContext->adaptationSetIdx = iAdaptationSet;
 						mNumberOfTracks = 1;
 						isIframeAdaptationAvailable = true;
+						if(!adaptationSet->GetContentProtection().empty())
+						{
+							encryptedIframeTrackPresent = true;
+							AAMPLOG_WARN("PrivateStreamAbstractionMPD::%s %d Detected encrypted iframe track", __FUNCTION__, __LINE__);
+						}
 						break;
 					}
 				}
@@ -6088,7 +6094,11 @@ void StreamAbstractionAAMP_MPD::StreamSelection( bool newTune, bool forceSpeedsC
 
 			ProcessContentProtection(period->GetAdaptationSets().at(selAdaptationSetIndex),(MediaType)i);
 			mNumberOfTracks++;
-		} // AAMP_NORMAL_PLAY_RATE
+		}
+		else if (AAMP_NORMAL_PLAY_RATE < rate && encryptedIframeTrackPresent) //Process content protection for encyrpted Iframe
+		{
+			ProcessContentProtection(period->GetAdaptationSets().at(pMediaStreamContext->adaptationSetIdx),(MediaType)i);
+		}
 
 		if(selAdaptationSetIndex < 0 && rate == 1)
 		{
