998dd1f1c128794ecc3ab56ce439e127b379c1d1
jcheer699 <JasirMohammed_Cheeroth@comcast.com>
2021-10-07 18:13:37 -0400

    XIONE-6423 : IP-Linear channel tune starts with jerky video and black screen flashes observed when subtitles enabled
    Reason for change: Turn on and mute subtitle by default
    Test Procedure: Test case will be updated in the ticket
    Risks: low
    
    Signed-off-by:  jasirmohammed_cheeroth@comcast.com 
    Change-Id: Ia47b2cde1ad19337a41e1afa8d16eeaca8f316d7
    (cherry picked from commit ff114e591f3293ed9a7a701657491a810cf77174)
    (cherry picked from commit fae9488ca8e4259981b423d66544df7286824982)

--- priv_aamp.cpp
+++ priv_aamp.cpp
@@ -8710,7 +8710,7 @@ int PrivateInstanceAAMP::GetAudioTrack()
  *   @param[in] trackId index of text track in available track list
  *   @return void
  */
-void PrivateInstanceAAMP::SetTextTrack(int trackId)
+void PrivateInstanceAAMP::SetTextTrack(int trackId, bool defaultEnable)
 {
 	if (mpStreamAbstractionAAMP)
 	{
@@ -8765,7 +8765,7 @@ void PrivateInstanceAAMP::SetTextTrack(int trackId)
 			else
 			{
 				//Unmute subtitles
-				SetCCStatus(true);
+				SetCCStatus(!defaultEnable); //if defualt enable, set the subtitle status mute
 
 				//TODO: Effective handling between subtitle and CC tracks
 				int textTrack = mpStreamAbstractionAAMP->GetTextTrack();
@@ -8773,15 +8773,16 @@ void PrivateInstanceAAMP::SetTextTrack(int trackId)
 				if (trackId != textTrack)
 				{
 					SetPreferredTextTrack(track);
-					discardEnteringLiveEvt = true;
-					
-					seek_pos_seconds = GetPositionMilliseconds()/1000.0;
-					AcquireStreamLock();
-					TeardownStream(false);
-					TuneHelper(eTUNETYPE_SEEK);
-					ReleaseStreamLock();
-
-					discardEnteringLiveEvt = false;
+					if(!defaultEnable) //avoid retune for default enable
+					{
+						discardEnteringLiveEvt = true;
+						seek_pos_seconds = GetPositionMilliseconds()/1000.0;
+						AcquireStreamLock();
+						TeardownStream(false);
+						TuneHelper(eTUNETYPE_SEEK);
+						ReleaseStreamLock();						
+						discardEnteringLiveEvt = false;
+					}
 				}
 			}
 		}
