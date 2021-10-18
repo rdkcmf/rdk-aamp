36edd9729b6b48d539ee86d4fbfbabae944328d4
jcheer699 <JasirMohammed_Cheeroth@comcast.com>
2021-10-18 13:09:15 -0400

    LLAMA-4070 : subtitles on S4C (171) channel at Sky are not shown - but with fog=0 they are found Reason for change: select en as the default sub lang & clean up to have default subtitle to work with multiple tracks Test Procedure: Test case will be updated in the ticket Risks: low
    Signed-off-by:  jasirmohammed_cheeroth@comcast.com 
    Change-Id: I674a22814a7fb59c5d4db56ba7921af521123cff
    (cherry picked from commit 32324fc76a2360159d3328552d4df8e05a0cb72c)

--- priv_aamp.cpp
+++ priv_aamp.cpp
@@ -8696,7 +8696,7 @@ int PrivateInstanceAAMP::GetAudioTrack()
  *   @param[in] trackId index of text track in available track list
  *   @return void
  */
-void PrivateInstanceAAMP::SetTextTrack(int trackId, bool defaultEnable)
+void PrivateInstanceAAMP::SetTextTrack(int trackId)
 {
 	if (mpStreamAbstractionAAMP)
 	{
@@ -8751,7 +8751,7 @@ void PrivateInstanceAAMP::SetTextTrack(int trackId, bool defaultEnable)
 			else
 			{
 				//Unmute subtitles
-				SetCCStatus(!defaultEnable); //if defualt enable, set the subtitle status mute
+				SetCCStatus(true);
 
 				//TODO: Effective handling between subtitle and CC tracks
 				int textTrack = mpStreamAbstractionAAMP->GetTextTrack();
@@ -8759,16 +8759,13 @@ void PrivateInstanceAAMP::SetTextTrack(int trackId, bool defaultEnable)
 				if (trackId != textTrack)
 				{
 					SetPreferredTextTrack(track);
-					if(!defaultEnable) //avoid retune for default enable
-					{
-						discardEnteringLiveEvt = true;
-						seek_pos_seconds = GetPositionMilliseconds()/1000.0;
-						AcquireStreamLock();
-						TeardownStream(false);
-						TuneHelper(eTUNETYPE_SEEK);
-						ReleaseStreamLock();						
-						discardEnteringLiveEvt = false;
-					}
+					discardEnteringLiveEvt = true;
+					seek_pos_seconds = GetPositionMilliseconds()/1000.0;
+					AcquireStreamLock();
+					TeardownStream(false);
+					TuneHelper(eTUNETYPE_SEEK);
+					ReleaseStreamLock();
+					discardEnteringLiveEvt = false;
 				}
 			}
 		}
