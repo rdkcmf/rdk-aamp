d59842307fb01beb67bd6e43d3b707482186ac76
Jaikumar Subramanian <jai_kumar@cable.comcast.com>
2021-11-22 15:56:54 +0000

    Revert "DELIA-50649: TSB is getting lost after trickplay"
    This reverts commit c1b5237f727958d66cd233ab2fbec67181de34d3.
    
    Reason for revert: Causing regression in Sprint
    
    Change-Id: I1ea401f2f7532f6e09c358a1ea4a4c47bc7237c8

--- fragmentcollector_mpd.cpp
+++ fragmentcollector_mpd.cpp
@@ -7470,10 +7470,7 @@ void StreamAbstractionAAMP_MPD::AdvanceTrack(int trackIdx, bool trickPlay, doubl
 							delta = rate / vodTrickplayFPS;
 						}
 						double currFragTime = pMediaStreamContext->fragmentTime;
-						if(rate > 0)
-						{
 						delta = SkipFragments(pMediaStreamContext, delta);
-						}
 						mBasePeriodOffset += (pMediaStreamContext->fragmentTime - currFragTime);
 					}
 					if (PushNextFragment(pMediaStreamContext, getCurlInstanceByMediaType(static_cast<MediaType>(trackIdx))))
@@ -8070,7 +8067,7 @@ void StreamAbstractionAAMP_MPD::FetcherLoop()
 
                                         }
 					int timeoutMs = refreshInterval - (int)(aamp_GetCurrentTimeMS() - mLastPlaylistDownloadTimeMs);
-					if( timeoutMs <= 0 && mIsLiveManifest )
+					if( timeoutMs <= 0 && mIsLiveManifest && rate > 0 )
 					{
 						liveMPDRefresh = true;
 						break;
