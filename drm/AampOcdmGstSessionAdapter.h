14e2b987ea5906e9e744f5fe0fd732b33d00e6b3
Jaikumar Subramanian <jai_kumar@cable.comcast.com>
2022-01-06 15:24:11 +0000

    Revert "XIONE-7312:[UK]"OCDMDecryptEX_video"debug print not present in logs when"
    This reverts commit bc58d9955a5fe51e069939dc09951a224e54fa4f.
    
    Reason for revert: Increased crash in sprint 
    
    Change-Id: I04ea22add0cc15ca18afa7f2313ca0a1795e9cc7

--- drm/AampOcdmGstSessionAdapter.h
+++ drm/AampOcdmGstSessionAdapter.h
@@ -10,18 +10,24 @@ class AAMPOCDMGSTSessionAdapter : public AAMPOCDMSessionAdapter
 #endif
 public:
 	AAMPOCDMGSTSessionAdapter(AampLogManager *logObj, std::shared_ptr<AampDrmHelper> drmHelper) : AAMPOCDMSessionAdapter(logObj, drmHelper)
+#if defined(AMLOGIC)
 , AAMPOCDMGSTSessionDecrypt(nullptr)
 	{
                 const char* ocdmgstsessiondecrypt = "opencdm_gstreamer_session_decrypt_ex";
                 AAMPOCDMGSTSessionDecrypt = (OpenCDMError(*)(struct OpenCDMSession*, GstBuffer*, GstBuffer*, const uint32_t, GstBuffer*, GstBuffer*, uint32_t, GstCaps*))dlsym(RTLD_DEFAULT, ocdmgstsessiondecrypt);
                 if (AAMPOCDMGSTSessionDecrypt)
-                        AAMPLOG_INFO("Has opencdm_gstreamer_session_decrypt_ex");
+                        logprintf("Has opencdm_gstreamer_session_decrypt_ex");
                 else
-                        AAMPLOG_WARN("No opencdm_gstreamer_session_decrypt_ex found");
+                        logprintf("No opencdm_gstreamer_session_decrypt_ex found");
+#else
+	{
+#endif
 	};
 	~AAMPOCDMGSTSessionAdapter() {};
 
 	int decrypt(GstBuffer* keyIDBuffer, GstBuffer* ivBuffer, GstBuffer* buffer, unsigned subSampleCount, GstBuffer* subSamplesBuffer, GstCaps* caps);
 private:
+#if defined(AMLOGIC)
         OpenCDMError(*AAMPOCDMGSTSessionDecrypt)(struct OpenCDMSession*, GstBuffer*, GstBuffer*, const uint32_t, GstBuffer*, GstBuffer*, uint32_t, GstCaps*);
+#endif
 };
