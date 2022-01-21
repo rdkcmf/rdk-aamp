6019869a09c68189e22486e15e6aa0fce0987308
Jakub Wojtasik <jakub.wojtasik@consult.red>
2022-01-21 11:29:46 +0100

    DELIA-53416 [aamp] [Linux] Create docker image or installer script for aamp
    Reason for change: add integrated support for local linux builds
    Test Procedure: use scripts to prepare linux build
    Risks: Low
    
    Signed-off-by: Jakub Wojtasik <jakub.wojtasik@consult.red>
    
    Change-Id: If5436f0fe5561e5a45f38059dff53ca95ad40326
    
    DELIA-53416 [aamp] [Linux] Create docker image or installer script for aamp
    
    Reason for change: fixed a small error with cmake
    Test Procedure: Please read the README in this directory, and follow the
    instructions in it.
    Risks: Low
    
    Signed-off-by: Harry Owens <harry.owens@sky.uk>
    
    RDKAAMP-15 : [Linux] Create docker image or installer script for aamp
    
    Reason for change: Adding some additional packages if you are running the script on Ubuntu 18:04 LTS to add support for this version of Ubuntu.
    Test Procedure: Follow instructions exactly in the readme. Test on both
    Ubuntu 20:04 LTS & Ubuntu 18:04 LTS.
    Risks: low
    
    Change-Id: I0faa8919fb276667fd49fa88aeeed25b6fe61ef6

---
+++ Linux/clean.sh
@@ -0,0 +1,8 @@
+rm -rf aampabr
+rm -rf aampmetrics
+rm -rf cJSON
+rm -rf gst-plugins-rdk-aamp
+rm -rf include
+rm -rf lib
+rm -rf libdash
+rm -rf ../build
---
+++ Linux/install-linux-deps.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+function package_exists_lin() {
+    dpkg -s "$1" &> /dev/null
+    return $?
+}
+
+function install_package() {
+    if ! package_exists_lin $1 ; then
+        echo "installing $1"
+        apt install $1 -y
+    fi
+}
+
+install_package git
+install_package cmake
+install_package gcc
+install_package g++
+install_package libcurl4-openssl-dev
+install_package libgstreamer1.0-dev 
+install_package libgstreamer-plugins-bad1.0-dev 
+install_package libssl-dev 
+install_package libxml2-dev 
+install_package pkg-config 
+install_package zlib1g-dev
+
+ver=$(grep -oP 'VERSION_ID="\K[\d.]+' /etc/os-release)
+
+if [ ${ver:0:2} -eq 20 ]; then
+	echo "No additional packages required. OS verion is $ver"
+elif [ ${ver:0:2} -eq 18 ]; then
+	echo "Two additional packages required. 0S version is $ver"
+	install_package freeglut3-dev
+	install_package libglew-dev
+else
+	echo "Please upgrade your Ubuntu version to at least 18:04 LTS. OS version is $ver"
+fi
