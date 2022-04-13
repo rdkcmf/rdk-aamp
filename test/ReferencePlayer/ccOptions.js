48f1b1844b2dea497207d6951df5b74f299ffe21
Manuchand K S <MANUCHAND_SUNILKUMAR@comcast.com>
2022-04-13 00:33:03 -0400

    DELIA-55405 : Enhance the ReferencePlayer to test more CC related test cases
    Reason for change: Provision to set cc style via ccOptions json object
    Risks: Low
    
    DEPS_BRANCH=rdk/components/generic/gst-plugins-rdk-aamp:dev_sprint_22_1
    DEPS_BRANCH=rdk/components/generic/aampabr:dev_sprint_22_1
    DEPS_BRANCH=rdk/components/generic/aampmetrics:dev_sprint_22_1
    
    Signed-off-by: Manuchand K S <MANUCHAND_SUNILKUMAR@comcast.com>
    Change-Id: Ie042cbb3e01529b237f6d59580ebf4c084356055

---
+++ test/ReferencePlayer/ccOptions.js
@@ -0,0 +1,17 @@
+var ccOptions = {
+    "penItalicized": false,
+    "textEdgeStyle": "none",
+    "textEdgeColor": "black",
+    "penSize": "small",
+    "windowFillColor": "black",
+    "fontStyle": "default",
+    "textForegroundColor": "white",
+    "windowFillOpacity": "transparent",
+    "textForegroundOpacity": "solid",
+    "textBackgroundColor": "black",
+    "textBackgroundOpacity": "solid",
+    "windowBorderEdgeStyle": "none",
+    "windowBorderEdgeColor": "black",
+    "penUnderline": false
+}
+   
