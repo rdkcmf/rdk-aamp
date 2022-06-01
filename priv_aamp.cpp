e7e338d2e1a4cb14f35a5155c595c4c4697b2681
Mark Towers <mark.towers@sky.uk>
2022-06-01 09:52:04 +0100

    XIONE-8379:quick FF->Play->FF leads to trickmode icon stuck spinning
    Reason for change: Make progress report timing more deterministic and allow position queries (and resulting reports) during the transition to the play state (which gst may be performing asynchronously).
    Test Procedure: see ticket
    Risks: low
    
    DEPS_BRANCH=rdk/components/generic/gst-plugins-rdk-aamp:dev_sprint_22_1
    DEPS_BRANCH=rdk/components/generic/aampabr:dev_sprint_22_1
    DEPS_BRANCH=rdk/components/generic/aampmetrics:dev_sprint_22_1
    
    Signed-off-by: Mark Towers <mark.towers@sky.uk>
    Change-Id: I650e3c566706a3f5ebb156abd1e7a1fe57a7e56e

--- priv_aamp.cpp
+++ priv_aamp.cpp
@@ -1806,8 +1806,6 @@ void PrivateInstanceAAMP::ReportProgress(bool sync, bool beginningOfStream)
 			bProcessEvent = false;
 		}
 
-		mReportProgressPosn = position;
-
 		if(ISCONFIGSET_PRIV(eAAMPConfig_UseAbsoluteTimeline) && ISCONFIGSET_PRIV(eAAMPConfig_InterruptHandling) && mTSBEnabled)
 		{
 			// Reporting relative positions for Fog TSB with interrupt handling
@@ -1849,9 +1847,12 @@ void PrivateInstanceAAMP::ReportProgress(bool sync, bool beginningOfStream)
 			{
 				mEventManager->SendEvent(evt);
 			}
-		}
 
-		mReportProgressTime = aamp_GetCurrentTimeMS();
+			//XIONE-8379 - these statements should be called together whenever a report is made AND ONLY when a report is made
+			//previously this was not the case and report sending could be erratic
+			mReportProgressPosn = position;
+			mReportProgressTime = aamp_GetCurrentTimeMS();
+		}
 	}
 }
 
