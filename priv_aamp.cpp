d589f592aefa97c900b4c7bb6d18fbf6d36ee402
Anurag Krishnan <Anurag_Krishnan@comcast.com>
2022-06-07 05:36:05 +0000

    DELIA-56354:Webprocess crash with ProcessPendingDiscontinuity
    Reason for change: crash is seen becuase during trick play operation, mpStreamAbstractionAAMP deleted from TearDownStream() from TuneHelper() and the same time when the ProcessPendingDiscontinuity() tried to access the mpStreamAbstractionAAMP object it crashed. Fix added to wait for discontinuity tune operation to finish before proceeding with teardown
    Test Procedure: Tested steps on ticket
    Risks: Low
    
    DEPS_BRANCH=rdk/components/generic/gst-plugins-rdk-aamp:dev_sprint_22_1
    DEPS_BRANCH=rdk/components/generic/aampabr:dev_sprint_22_1
    DEPS_BRANCH=rdk/components/generic/aampmetrics:dev_sprint_22_1
    
    Signed-off-by: Anurag Krishnan <akrish513@cable.comcast.com>
    Change-Id: Id4a82b2b8c242b5d64cb50b06a2d6d18b72c1e0b

--- priv_aamp.cpp
+++ priv_aamp.cpp
@@ -4455,9 +4455,10 @@ void PrivateInstanceAAMP::TeardownStream(bool newTune)
 		}
 		if (waitForDiscontinuityProcessing)
 		{
-			//wait for discont tune operation to finish before proceeding with stop
+			//wait for discontinuity tune operation to finish before proceeding with stop
 			if (mDiscontinuityTuneOperationInProgress)
 			{
+				AAMPLOG_WARN("TeardownStream invoked while mDiscontinuityTuneOperationInProgress set. Wait until the Discontinuity Tune operation to complete!!");
 				pthread_cond_wait(&mCondDiscontinuity, &mLock);
 			}
 			else
@@ -4468,10 +4469,18 @@ void PrivateInstanceAAMP::TeardownStream(bool newTune)
 		}
 	}
 	// Maybe mDiscontinuityTuneOperationId is 0, ProcessPendingDiscontinuity can be invoked from NotifyEOSReached too
-	else if (mProgressReportFromProcessDiscontinuity)
+	else if (mProgressReportFromProcessDiscontinuity || mDiscontinuityTuneOperationInProgress)
 	{
-		AAMPLOG_WARN("TeardownStream invoked while mProgressReportFromProcessDiscontinuity set!");
-		mDiscontinuityTuneOperationInProgress = false;
+		if(mDiscontinuityTuneOperationInProgress)
+		{
+			AAMPLOG_WARN("TeardownStream invoked while mDiscontinuityTuneOperationInProgress set. Wait until the pending discontinuity tune operation to complete !!");
+			pthread_cond_wait(&mCondDiscontinuity, &mLock);
+		}
+		else
+		{
+			AAMPLOG_WARN("TeardownStream invoked while mProgressReportFromProcessDiscontinuity set!");
+			mDiscontinuityTuneOperationInProgress = false;
+		}
 	}
 
 	//reset discontinuity related flags
