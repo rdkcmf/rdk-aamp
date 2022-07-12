13483976b17670d6cd5d481e6348af5cfea8d49e
dathia184 <dhanapal_athiappagounder@comcast.com>
2022-07-12 18:18:41 +0000

    RDK-29319 RDK-37340: [AAMP] Parallel download for Playlist Refresh
    Reason for change: To bring back parallel playlist download changes into sprint branch along with stability issue fixes
    Test Procedure: To test with linear and VOD playback on comcas, peacocok and all supported apps on all use cases and refer ticket for more test details
    Risks: low
    
    DEPS_BRANCH=rdk/components/generic/gst-plugins-rdk-aamp:dev_sprint_22_1
    DEPS_BRANCH=rdk/components/generic/aampabr:dev_sprint_22_1
    DEPS_BRANCH=rdk/components/generic/aampmetrics:dev_sprint_22_1
    
    Change-Id: I0fc8fdb899fc53a236b5aa905bad0d30c22eedf2
    Signed-off-by: Dhanapal A <dhanapal_athiappagounder@comcast.com>
    
    RDK-37340: Parallel download for Playlist Refresh
    
    Reason for change: Added fix to avoid memory leak issue in DASH linear streams and mpd period parsing optimization to avoid duplicate parsing
    Test Procedure: To test with peacocok dash SLE playback with extended dvr window size on more than 3 hours and ensure no hang or crash observed
    Risks: low
    
    DEPS_BRANCH=rdk/components/generic/gst-plugins-rdk-aamp:dev_sprint_22_1
    DEPS_BRANCH=rdk/components/generic/aampabr:dev_sprint_22_1
    DEPS_BRANCH=rdk/components/generic/aampmetrics:dev_sprint_22_1
    
    Signed-off-by: Dhanapal A <dhanapal_athiappagounder@comcast.com>
    Change-Id: I1334ebe323ed562505d4ed40a53e053629aee947
    
    RDK-37340: [AAMP] Parallel download for Playlist Refresh
    
    Reason for change: During fragment collector get file curl waiting time, playlist downloader thread modified fragment sequence number reference and next push fragment looping on either audio/video downloader thread reset playlist downloaded status then other fragment downloader taking wrong sequence on next fragment request failed and
    with wrong period end time. Now added fix to maintain different playlist update status as each media track context to avoid time sync issue
    
    Test Procedure: To test with DASH linear content on both comcast, peacock streams for more than 5 hours to ensure not seeing any discontinuity issues
    Risks: low
    
    DEPS_BRANCH=rdk/components/generic/gst-plugins-rdk-aamp:dev_sprint_22_1
    DEPS_BRANCH=rdk/components/generic/aampabr:dev_sprint_22_1
    DEPS_BRANCH=rdk/components/generic/aampmetrics:dev_sprint_22_1
    
    Signed-off-by: Dhanapal A <dhanapal_athiappagounder@comcast.com>
    with '#' will be ignored, and an empty message aborts the commit.  #
    
    Change-Id: Ie751b1d7c2ba3313fe83ea0fe14b3431f0ac582a

--- fragmentcollector_mpd.h
+++ fragmentcollector_mpd.h
@@ -382,6 +382,17 @@ public:
 	 * @param[out] tTracks text tracks
 	 */
 	void ParseTrackInformation(IAdaptationSet *adaptationSet, uint32_t iAdaptationIndex, MediaType media, std::vector<AudioTrackInfo> &aTracks, std::vector<TextTrackInfo> &tTracks);
+	bool mperiodChanged[AAMP_TRACK_COUNT];
+	bool mpendingPtoProcessing[AAMP_TRACK_COUNT];
+	double mtempDelta[AAMP_TRACK_COUNT];
+	double mFirstBufferScaledPts[AAMP_TRACK_COUNT];
+	uint64_t mLastPlaylistDownloadTimeMs; // Last playlist refresh time
+	/**
+	 * @fn ProcessPlaylist
+	 * @param newPlaylist buffer
+	 * @param[out] http_error code
+	 */
+	void ProcessPlaylist(GrowableBuffer& newPlaylist, long http_error);
 private:
 	/**
 	 * @fn printSelectedTrack
@@ -427,6 +438,10 @@ private:
 	 * @param root: XML root node
 	 */ 
 	bool FindServerUTCTime(Node* root);
+	/**
+	 * @fn FetchDashManifest
+	 */
+	AAMPStatusType FetchDashManifest();
 	/**
 	 * @fn FindTimedMetadata
 	 * @param mpd MPD top level element
@@ -530,7 +545,7 @@ private:
 	/**
 	 * @fn UpdateTrackInfo
 	 */
-	AAMPStatusType UpdateTrackInfo(bool modifyDefaultBW, bool periodChanged, bool resetTimeLineIndex=false);
+	AAMPStatusType UpdateTrackInfo(bool modifyDefaultBW, bool resetTimeLineIndex=false);
 	/**
 	 * @fn SkipFragments
 	 * @param pMediaStreamContext Media track object
@@ -562,11 +577,11 @@ private:
 	/**
 	 * @fn GetCulledSeconds
 	 */
-	double GetCulledSeconds();
+	double GetCulledSeconds(std::vector<PeriodInfo> &currMPDPeriodDetails);
 	/**
 	 * @fn UpdateCulledAndDurationFromPeriodInfo
 	 */
-	void UpdateCulledAndDurationFromPeriodInfo();
+	void UpdateCulledAndDurationFromPeriodInfo(std::vector<PeriodInfo> &currMPDPeriodDetails);
 	/**
 	 * @fn UpdateLanguageList
 	 * @return void
@@ -603,13 +618,13 @@ private:
 	 */
 	std::string GetLanguageForAdaptationSet( IAdaptationSet *adaptationSet );
 	/**
-	 * @fn GetMpdFromManfiest
+	 * @fn GetMpdFromManifest
 	 * @param manifest buffer pointer
 	 * @param mpd MPD object of manifest
 	 * @param manifestUrl manifest url
 	 * @param init true if this is the first playlist download for a tune/seek/trickplay
 	 */
-	AAMPStatusType GetMpdFromManfiest(const GrowableBuffer &manifest, MPD * &mpd, std::string manifestUrl, bool init = false);
+	AAMPStatusType GetMpdFromManifest(const GrowableBuffer &manifest, MPD * &mpd, std::string manifestUrl, bool init);
 	/**
 	 * @fn GetDrmPrefs
 	 * @param The UUID for the DRM type
@@ -632,19 +647,6 @@ private:
 	 * @param isFogPeriod true if it is fog period
 	 */
 	bool IsEmptyAdaptation(IAdaptationSet *adaptationSet, bool isFogPeriod);
-	/**
-	 * @fn GetAvailableVSSPeriods
-	 * @param PeriodIds vector of new Early Available Periods
-	 */
-	void GetAvailableVSSPeriods(std::vector<IPeriod*>& PeriodIds);
-	/**
-	 * @fn CheckForVssTags
-	 */
-	bool CheckForVssTags();
-	/**
-	 * @fn GetVssVirtualStreamID
-	 */
-	std::string GetVssVirtualStreamID();
 	/**
 	 * @fn IsMatchingLanguageAndMimeType
 	 * @param[in] type - media type
@@ -729,6 +731,10 @@ private:
 	class MediaStreamContext *mMediaStreamContext[AAMP_TRACK_COUNT];
 	int mNumberOfTracks;
 	int mCurrentPeriodIdx;
+	size_t mNumberOfPeriods;	// Number of periods in the updated manifest
+	unsigned mIterPeriodIndex;	// FetcherLoop period iterator index
+	unsigned mUpperBoundaryPeriod;	// Last playable period index
+	unsigned mLowerBoundaryPeriod;	// First playable period index
 	double mEndPosition;
 	bool mIsLiveStream;    	    	   /**< Stream is live or not; won't change during runtime. */
 	bool mIsLiveManifest;   	   /**< Current manifest is dynamic or static; may change during runtime. eg: Hot DVR. */
@@ -743,7 +749,6 @@ private:
 	uint64_t mMinUpdateDurationMs;
 	double mTSBDepth;
 	double mPresentationOffsetDelay;
-	uint64_t mLastPlaylistDownloadTimeMs;
 	double mFirstPTS;
 	double mStartTimeOfFirstPTS;
 	double mVideoPosRemainder;
@@ -751,6 +756,8 @@ private:
 	AudioType mAudioType;
 	int mPrevAdaptationSetCount;
 	std::vector<long> mBitrateIndexVector;
+	bool playlistDownloaderThreadStarted; // Playlist downloader thread start status
+
 	// In case of streams with multiple video Adaptation Sets, A profile
 	// is a combination of an Adaptation Set and Representation within
 	// that Adaptation Set. Hence we need a mapping from a profile to
@@ -783,6 +790,7 @@ private:
 	double mDeltaTime;
 	double mHasServerUtcTime;
 	bool mLiveTimeFragmentSync;
+	std::mutex playlistMutex;       /**< Mutex locked for accessing and updating mpd document */
  	/**
 	 * @fn GetPeriodStartTime
 	 * @param mpd : pointer manifest
@@ -825,6 +833,18 @@ private:
 	 * @fn FindPeriodGapsAndReport
 	 */
 	void FindPeriodGapsAndReport();
+	/**
+	 * @fn IndexNewMPDDocument
+	 */
+	void IndexNewMPDDocument(bool updateTrackInfo = true);
+	/**
+	 * @fn AcquirePlaylistLock
+	 */
+	void AcquirePlaylistLock();
+	/**
+	 * @fn ReleasePlaylistLock
+	 */
+	void ReleasePlaylistLock();
 	
 #ifdef AAMP_MPD_DRM
 	/**
@@ -849,6 +869,24 @@ private:
 	 */
 	std::shared_ptr<AampDrmHelper> CreateDrmHelper(IAdaptationSet * adaptationSet,MediaType mediaType);
 #endif
+	/**
+	* @fn CheckForVssTags
+	*/
+	bool CheckForVssTags();
+	/**
+	* @fn ProcessVssLicenseRequset
+	*/
+	void ProcessVssLicenseRequset();
+	/**
+	* @fn GetAvailableVSSPeriods
+	* @param PeriodIds VSS Periods
+	*/
+	void GetAvailableVSSPeriods(std::vector<IPeriod*>& PeriodIds);
+	/**
+	* @fn GetVssVirtualStreamID
+	*/
+	std::string GetVssVirtualStreamID();
+
 	std::vector<StreamInfo*> thumbnailtrack;
 	std::vector<TileInfo> indexedTileInfo;
 	double mFirstPeriodStartTime; /*< First period start time for progress report*/
