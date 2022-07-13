9c50c13b333ee0d0878d7c91329c0a398d7b2994
Stephen Waddell <stephen.waddell@consult.red>
2022-07-13 11:41:03 +0000

    XIONE-8830: After scrub, subtitle not in sync with AV
    Reason for change: When audio playtarget is edited for
    segments of different duration, subtitle needs to be
    adjusted also
    Test Procedure: Skip on IT/DE HLS platforms and check
    subtitles in sync with AV
    Risks: Low
    
    DEPS_BRANCH=rdk/components/generic/aampabr:dev_sprint_22_1
    DEPS_BRANCH=rdk/components/generic/aampmetrics:dev_sprint_22_1
    DEPS_BRANCH=rdk/components/generic/gst-plugins-rdk-aamp:dev_sprint_22_1
    
    Signed-off-by: Stephen Waddell <stephen.waddell@consult.red>
    Change-Id: I20517e93be9cf720933ef862c2b8ea83bf762329

--- fragmentcollector_hls.cpp
+++ fragmentcollector_hls.cpp
@@ -5101,6 +5101,7 @@ AAMPStatusType StreamAbstractionAAMP_HLS::Init(TuneType tuneType)
 				{
 					AAMPLOG_TRACE("Setting audio playtarget %f to video playtarget %f", audio->playTarget, ts->playTarget);
 					audio->playTarget = ts->playTarget;
+					subtitle->playTarget = ts->playTarget;
 				}
 			}
 			if (IsLive() && audio->enabled && !ISCONFIGSET(eAAMPConfig_AudioOnlyPlayback))
