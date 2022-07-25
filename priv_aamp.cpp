0ecc3831a729a946676ff57fc6657777bf9bb067
nkasi944 <nithya_kasi@comcast.com>
2022-07-25 19:39:11 +0530

    DELIA-57026: [Field]Crash with TeardownStream
    Reason for change: Mutex was not signaled when the discontinuity process was called from NotifyEOSReached().
    Test Procedure: As per mentioned in the comments
    Risks: Low
    
    Signed-off-by: nkasi944 <nithya_kasi@comcast.com>
    Change-Id: I8363afe1e101bc4518d337d87241d1b9d7875d5e
    (cherry picked from commit 21cd00f063e5d54d418bf55ef93f6be0faf13b6a)

--- priv_aamp.cpp
+++ priv_aamp.cpp
@@ -400,9 +400,10 @@ static gboolean PrivateInstanceAAMP_ProcessDiscontinuity(gpointer ptr)
 		{
 			aamp->SyncBegin();
 			aamp->mDiscontinuityTuneOperationId = 0;
-			pthread_cond_signal(&aamp->mCondDiscontinuity);
 			aamp->SyncEnd();
 		}
+		pthread_cond_signal(&aamp->mCondDiscontinuity);
+
 	}
 	return G_SOURCE_REMOVE;
 }
@@ -2718,6 +2719,7 @@ void PrivateInstanceAAMP::NotifyEOSReached()
 	else
 	{
 		ProcessPendingDiscontinuity();
+		pthread_cond_signal(&mCondDiscontinuity);
 		DeliverAdEvents();
 		AAMPLOG_WARN("PrivateInstanceAAMP:  EOS due to discontinuity handled");
 	}
