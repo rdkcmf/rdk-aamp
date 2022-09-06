f3c3f4cd98e83ca4a80c5ba0ee93d498994eb534
Manuchand K S <MANUCHAND_SUNILKUMAR@comcast.com>
2022-09-06 13:22:03 -0400

    DELIA-57888 : Add new field current fragment bitrate in HttpRequestEnd
    Reason for change: Add the currentBitrate in HttpRequestEnd log.
    Test Procedure: Test cases defined in the ticket.
    Risks: Low
    
    Signed-off-by: Manuchand K S <MANUCHAND_SUNILKUMAR@comcast.com>
    Change-Id: I4f33262a053c52497a44972da4259288a067228a
    (cherry picked from commit c3a7648fbaee7e85e17476533f762690558047a0)

--- test/DownloadActivityViewer/parseutils.js
+++ test/DownloadActivityViewer/parseutils.js
@@ -5,50 +5,85 @@ var months =
     "Jul","Aug","Sep",
     "Oct","Nov","Dec"
 ];
+
 function ParseReceiverLogTimestamp( line )
 {
-    var marker = "[AAMP-PLAYER]";
-    var markerOffset = line.indexOf(marker);
-    if( markerOffset<0 ) return null; // ignore - not AAMP-related
-    
-    line = line.substr(0,markerOffset);
-    var part = line.split(":");
-    if( part.length==3 )
-    { // "<sec>:<ms> : " format used in simulator
-        var utc = parseInt(part[0])*1000+parseInt(part[1]);
-        return utc;
-    }
-    
-    // format used on settop (GMT time)
-    part = line.split(" ");
-    var idx = 0;
-    var year = parseInt(part[idx++]);
-    if( isNaN(year) )
-    { // year missing (fog logs)
-        year = new Date().getFullYear();
-        idx--;
-    }
-    var month = months.indexOf(part[idx++]);
-    var day = parseInt(part[idx++]);
-    var time = part[idx++].split(":");
-    var hour = parseInt(time[0]);
-    var minute = parseInt(time[1]);
-    var seconds = parseFloat(time[2]);
-    var s = Math.floor(seconds);
-    var ms = Math.floor((seconds - s)*1000);
-    return Date.UTC(year, month, day, hour, minute, s, ms );
+	try
+	{
+		part = line.split(" ");
+		if( part[1]==":" )
+		{ // simulator format - <sec>:<ms>
+			part = part[0].split(":");
+			var s = parseInt(part[0]);
+			var ms = parseInt(part[1]);
+			return s*1000+ms;
+		}
+		else if( part[2]==":" )
+		{
+			var s = parseInt(part[0]);
+			var ms = parseInt(part[1]);
+			return s*1000+ms;
+		}
+
+		// format used on settop (GMT time)
+		var idx = 0;
+		var year = parseInt(part[idx++]);
+		if( isNaN(year) )
+		{ // year missing (fog logs)
+			year = new Date().getFullYear();
+			idx--;
+		}
+		var month = months.indexOf(part[idx++]);
+		var day = parseInt(part[idx++]);
+		var time = part[idx++].split(":");
+		var hour = parseInt(time[0]);
+		var minute = parseInt(time[1]);
+		var seconds = parseFloat(time[2]);
+		var s = Math.floor(seconds);
+		var ms = Math.floor((seconds - s)*1000);
+		return Date.UTC(year, month, day, hour, minute, s, ms );
+	}
+	catch( e )
+	{
+		//console.log( "ParseReceiverLogTimestamp error with: <LINE>" + line + "</LINE>" );
+		return 0;
+	}
 }
 
 // format includes %% for numbers and strings to be extracted, in-between other literal text
 // returns array of extracted values, or null if literal delimiters don't match
 function sscanf( string, format )
 {
+	string = string.trim();
+	var first = true;
     var rc = [];
     for(;;)
     {
         var nextParam = format.indexOf("%%");
-        if( nextParam<0 ) return rc; // done
-        if( string.substr(0,nextParam)!=format.substr(0,nextParam) )
+        if( nextParam<0 )
+		{ // no more parameters
+			if( first )
+			{
+				if( string.indexOf(format)<0 )
+				{
+					rc = null;
+				}
+			}
+			return rc;
+		}
+		var delim = format.substr(0,nextParam);
+		if( first )
+		{
+			nextParam = string.indexOf(delim);
+			if( nextParam<0 )
+			{
+				return null;
+			}
+			first = false;
+			string = string.substr(nextParam); // strip wildcard leading characters
+			continue;
+		}
+        else if( string.substr(0,nextParam)!=delim )
         {
             return null;
         }
@@ -69,7 +104,7 @@ function sscanf( string, format )
 }
 
 function Pad( num )
-{ // convert to fixed width by padding from left with spaces
+{ // used for formatting - convert to fixed width by padding from left with spaces
     num = Math.round(num);
     if( num<10 ) return "      "+num;
     if( num<100 ) return "     "+num;
@@ -78,6 +113,35 @@ function Pad( num )
     if( num<100000 ) return "  "+num;
     if( num<1000000 ) return " "+num;
     return num;
+}
+
+function Pad2( n )
+{ // prefix 2 digit number with leading zeros
+	if( n<10 ) return "0" + n;
+	return n;
+}
+
+function Pad3( n )
+{ // prefix 3 digit number with leading zeros
+	if( n<10 ) return "00" + n;
+	if( n<100 ) return "0" + n;
+	return n;
+}
+
+function FormatTime( duration )
+{
+	var ms = duration%1000;
+	duration = (duration-ms)/1000;
+	var s = duration%60;
+	duration = (duration-s)/60;
+	var m = duration%60;
+	duration = (duration-m)/60;
+	var h = duration;
+	var rc = [];
+	if( h>0 ){ rc.push(h); rc.push(Pad2(m)); rc.push(Pad2(s)); }
+	else if( m>0 ){ rc.push(m); rc.push(Pad2(s)); }
+	else{ rc.push(s); }
+	return rc.join(":");
 }
 
 var httpCurlErrMap =
@@ -104,6 +168,7 @@ var httpCurlErrMap =
 
 function mapError( code )
 {
+	code = parseInt(code);
     var desc = httpCurlErrMap[code];
     if( desc!=null )
     {
@@ -115,3 +180,268 @@ function mapError( code )
     }
     return ((code<100)?"CURL":"HTTP") + code + desc;
 }
+
+const eMEDIATYPE_VIDEO = 0;
+const eMEDIATYPE_AUDIO = 1;
+const eMEDIATYPE_SUBTITLE = 2;
+const eMEDIATYPE_AUX_AUDIO = 3;
+const eMEDIATYPE_MANIFEST = 4;
+const eMEDIATYPE_LICENSE = 5;
+const eMEDIATYPE_IFRAME = 6;
+const eMEDIATYPE_INIT_VIDEO = 7;
+const eMEDIATYPE_INIT_AUDIO = 8;
+const eMEDIATYPE_INIT_SUBTITLE = 9;
+const eMEDIATYPE_INIT_AUX_AUDIO = 10;
+const eMEDIATYPE_PLAYLIST_VIDEO = 11;
+const eMEDIATYPE_PLAYLIST_AUDIO = 12;
+const eMEDIATYPE_PLAYLIST_SUBTITLE = 13;
+const eMEDIATYPE_PLAYLIST_AUX_AUDIO = 14;
+const eMEDIATYPE_PLAYLIST_IFRAME = 15;
+const eMEDIATYPE_INIT_IFRAME = 16;
+const eMEDIATYPE_DSM_CC = 17;
+const eMEDIATYPE_IMAGE = 18;
+
+const ContentType_UNKNOWN = 0;
+const ContentType_CDVR = 1;
+const ContentType_VOD = 2;
+const ContentType_LINEAR = 3;
+const ContentType_IVOD = 4;
+const ContentType_EAS = 5;
+const ContentType_CAMERA = 6;
+const ContentType_DVR = 7;
+const ContentType_MDVR = 8;
+const ContentType_IPDVR = 9;
+const ContentType_PPV = 10;
+const ContentType_OTT = 11;
+const ContentType_OTA = 12;
+const ContentType_HDMIIN = 13;
+const ContentType_COMPOSITEIN = 14;
+const ContentType_SLE = 15;
+
+var mediaTypes = [
+"VIDEO",
+"AUDIO",
+"SUBTITLE",
+"AUX_AUDIO",
+"MANIFEST",
+"LICENSE",
+"IFRAME",
+"INIT_VIDEO",
+"INIT_AUDIO",
+"INIT_SUBTITLE",
+"INIT_AUX_AUDIO",
+"PLAYLIST_VIDEO",
+"PLAYLIST_AUDIO",
+"PLAYLIST_SUBTITLE",
+"PLAYLIST_AUX_AUDIO",
+"PLAYLIST_IFRAME",
+"PLAYLIST_INIT_IFRAME",
+"DSM_CC",
+"IMAGE" ];
+
+function ParseHttpRequestEnd( line )
+{
+    var rc = null;
+    var prefix = "HttpRequestEnd:";
+    var offs = line.indexOf(prefix);
+	if( offs>=0 )
+	{
+		var json = line.substr(offs+prefix.length);
+		try
+		{
+			rc = JSON.parse(json);
+		}
+		catch( err )
+		{
+			var param = json.split(",");
+            var fields = "";
+            if( param[0] != parseInt(param[0]) )
+            {
+                if( param.length == 16) {
+                    // If the log is old and don't have current bitrate field, use downloadbps value as br
+                    fields = "mediaType,type,responseCode,curlTime,total,connect,startTransfer,resolve,appConnect,preTransfer,redirect,dlSz,ulSz,br,url";
+                } else if( param.length == 17) {
+                    fields = "mediaType,type,responseCode,curlTime,total,connect,startTransfer,resolve,appConnect,preTransfer,redirect,dlSz,ulSz,downloadbps,br,url";
+                }
+                console.log('appName');
+                fields = "appName,"+fields;
+            } else {
+                if( param.length == 15) {
+                    // If the log is old and don't have current bitrate field, use downloadbps value as br
+                    fields = "mediaType,type,responseCode,curlTime,total,connect,startTransfer,resolve,appConnect,preTransfer,redirect,dlSz,ulSz,br,url";
+                } else if( param.length == 16) {
+                    fields = "mediaType,type,responseCode,curlTime,total,connect,startTransfer,resolve,appConnect,preTransfer,redirect,dlSz,ulSz,downloadbps,br,url";
+                }
+            }
+			fields = fields.split(",");
+			var httpRequestEnd = {};
+			for( var i=0; i<fields.length; i++ )
+			{
+				httpRequestEnd[fields[i]] = param[i];
+			};
+			httpRequestEnd.type = parseInt(httpRequestEnd.type);
+			httpRequestEnd.responseCode = parseInt(httpRequestEnd.responseCode);
+			httpRequestEnd.curlTime = parseFloat(httpRequestEnd.curlTime);
+			return httpRequestEnd;
+		}
+	}
+	
+	prefix = "HttpLicenseRequestEnd:";
+	offs = line.indexOf(prefix);
+	if( offs>=0 )
+	{
+		var json = line.substr(offs+prefix.length);
+		try
+		{
+			rc = JSON.parse(json);
+			rc.url = rc.license_url;
+			rc.type = eMEDIATYPE_LICENSE;
+		}
+		catch( err )
+		{
+			console.log( "ignoring corrupt JSON from receiver log<JSON>" + json + "</JSON>" );
+		}
+	}
+    return rc;
+}
+
+function ParseABRProfileBandwidth( line )
+{
+    var rc = null;
+    var prefix = "Added Video Profile to ABR BW= ";
+    var offs = line.indexOf(prefix);
+	if( offs>=0 )
+	{
+		var text = line.substr(offs+prefix.length);
+		const textArray = text.split(" ");
+
+		return textArray[0];
+	}
+	
+    return rc;
+}
+
+function ParseCurrentBitrate( line )
+{
+    var rc = null;
+    var prefix = "NotifyBitRateChangeEvent :: bitrate:";
+    var offs = line.indexOf(prefix);
+	if( offs>=0 )
+	{
+		var text = line.substr(offs+prefix.length);
+		const textArray = text.split(" ");
+
+		return textArray[0];
+	}
+	
+    return rc;
+}
+
+var contentTypeString = [
+"UNKNOWN",
+"CDVR",
+"VOD",
+"LINEAR",
+"IVOD",
+"EAS",
+"CAMERA",
+"DVR",
+"MDVR",
+"IPDVR",
+"PPV",
+"OTT",
+"OTA",
+"HDMIIN",
+"COMPOSITEIN",
+"SLE"];
+
+var trackNames = [ "video", "audio", "subtitle" ];
+
+var playerStates = [ // PrivAAMPState
+ "eSTATE_IDLE",
+ "eSTATE_INITIALIZING",
+ "eSTATE_INITIALIZED",
+ "eSTATE_PREPARING",
+ "eSTATE_PREPARED",
+ "eSTATE_BUFFERING",
+ "eSTATE_PAUSED",
+ "eSTATE_SEEKING",
+ "eSTATE_PLAYING",
+ "eSTATE_STOPPING",
+ "eSTATE_STOPPED",
+ "eSTATE_COMPLETE",
+ "eSTATE_ERROR",
+ "eSTATE_RELEASED",
+ "eSTATE_BLOCKED",
+];
+
+var eventNames = [
+ null,
+ "tuned",
+ "tuneFailed",
+ "speedChanged",
+ "eos",
+ "playlistIndexed",
+ "progress",
+ "decoderAvail", // AAMP_EVENT_CC_HANDLE_RECEIVED
+ "jsEvent",
+ "metadata",
+ "enteringLive",
+ "bitrateChanged",
+ "timedMetadata",
+ "bulkTimedMetadata",
+ "statusChanged",
+ "speedsChanged",
+ "seeked",
+ "tuneprofiling",
+ "bufferingChanged",
+ "durationChanged",
+ "audioTracksChanged",
+ "textTracksChanged",
+ "adBreaksChanged",
+ "adStarted",
+ "adCompleted",
+ "drmMetadata",
+ "anomaly",
+ "webVttCueData",
+ "adResolved",
+ "adReservationStart",
+ "adReservationEnd",
+ "adPlacementStart",
+ "adPlacementEnd",
+ "adPlacementError",
+ "adPlacementProgress",
+ "metricsData",
+ "id3Mmetadata",
+ "drmMessage",
+ "blocked",
+];
+
+var bitrateChangedReason =
+	[
+	"ABR:bandwidth", // eAAMP_BITRATE_CHANGE_BY_ABR
+	"Rampdown:failure", // eAAMP_BITRATE_CHANGE_BY_RAMPDOWN
+	"Tune:ABR reset", // eAAMP_BITRATE_CHANGE_BY_TUNE
+	"Seek:ABR reset", // eAAMP_BITRATE_CHANGE_BY_SEEK
+	"Trickplay:ABR ", // eAAMP_BITRATE_CHANGE_BY_TRICKPLAY
+	"Rampup - full buf", // eAAMP_BITRATE_CHANGE_BY_BUFFER_FULL
+	"Rampdown - empty buf", // eAAMP_BITRATE_CHANGE_BY_BUFFER_EMPTY
+	"ABR:FOG", // eAAMP_BITRATE_CHANGE_BY_FOG_ABR
+	"ABR:OTA", // eAAMP_BITRATE_CHANGE_BY_OTA
+	"ABR:HDMIIN", // eAAMP_BITRATE_CHANGE_BY_HDMIIN
+	 ];
+
+// various indices into IP_AAMP_TUNETIME
+const _tuneStartBaseUTCMS = 2;
+const _drmLicenseAcqStartTime = 26;
+const _drmLicenseAcqTotalTime = 27;
+const _drmFailErrorCode = 28;
+const _LicenseAcqPreProcessingDuration = 29;
+const _LicenseAcqNetworkDuration = 30;
+const _LicenseAcqPostProcDuration = 31;
+const _VideoFragmentDecryptDuration = 32;
+const _AudioFragmentDecryptDuration = 33;
+const _gstPlayStartTime = 34;
+const _gstFirstFrameTime = 35;
+const _tuneSuccess = 44;
+const _contentType = 36;
