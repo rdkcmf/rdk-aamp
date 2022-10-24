63535d4e0f70ee785c3ffc73d0ff405d87c66db2
Andrew Thornton <anthornt@synamedia.com>
2022-10-24 09:37:37 +0100

    RDKAAMP-292: extend existing generated test stream support to support playback as live events
    Reason for change: Add a test web server which can modify on the fly
    video test stream files to emulate event and live streams.
    Test Procedure: Run AAMP on Ubuntu to play the video test stream as
    described in README.txt.
    Risks: Low.
    
    DEPS_BRANCH=rdk/components/generic/gst-plugins-rdk-aamp:dev_sprint_22_1
    DEPS_BRANCH=rdk/components/generic/aampabr:dev_sprint_22_1
    DEPS_BRANCH=rdk/components/generic/aampmetrics:dev_sprint_22_1
    
    Signed-off-by: anthornt@synamedia.com
    Change-Id: Id1ddec68686c7aa5bfe20604321dc19aefea9efc

--- test/VideoTestStream/startserver.sh
+++ test/VideoTestStream/startserver.sh
@@ -1,8 +1,5 @@
-python3 -m http.server 8080
+python3 server.py "$@"
 # http://127.0.0.1:8080/main.m3u8
 # http://127.0.0.1:8080/main.mpd
 # http://127.0.0.1:8080/main_mp4.m3u8
-# http://127.0.0.1:8080/main_mp4.m3u8
-# http://127.0.0.1:8080/main_mux.m3u3
-
 
