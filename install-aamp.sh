1ace092aaa4afcce80abd611c763a77eb35ef52a
Vinish K B <Vinish_KorasseryBalakrishnan@comcast.com>
2022-10-26 10:02:46 +0530

    DELIA-58135: [AAMP][Linux] GetPositionMilliseconds gives wrong position if called immediately after seek
    Reason for change: Add a new cmake macro CMAKE_CUSTOM_QTDEMUX_ENABLED to selectively enable
    the qtdemux override macros in AAMP. This macros should be enabled only when qtdemux with AAMP
    patches are available.
    Test Procedure: Test in Ubuntu with progress logging enabled, and check the progress values after seek
    Risks: Low, impacts only Ubuntu and OSX simulators
    
    DEPS_BRANCH=rdk/components/generic/gst-plugins-rdk-aamp:dev_sprint_22_1
    DEPS_BRANCH=rdk/components/generic/aampabr:dev_sprint_22_1
    DEPS_BRANCH=rdk/components/generic/aampmetrics:dev_sprint_22_1
    
    Signed-off-by: Vinish K B <Vinish_KorasseryBalakrishnan@comcast.com>
    Change-Id: Id4265d80a3bc87bbfc22bfefdc541bb609e23683

--- install-aamp.sh
+++ install-aamp.sh
@@ -313,7 +313,7 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 
     mkdir -p build
     
-    cd build && PKG_CONFIG_PATH=/usr/local/opt/ossp-uuid/lib/pkgconfig:/usr/local/opt/libffi/lib/pkgconfig:/Library/Frameworks/GStreamer.framework/Versions/1.0/lib/pkgconfig:/usr/local/ssl/lib/pkgconfig:/usr/local/opt/curl/lib/pkgconfig:/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH cmake -DCMAKE_OSX_SYSROOT="/" -DCMAKE_OSX_DEPLOYMENT_TARGET="" -DSMOKETEST_ENABLED=ON -G Xcode ../
+    cd build && PKG_CONFIG_PATH=/usr/local/opt/ossp-uuid/lib/pkgconfig:/usr/local/opt/libffi/lib/pkgconfig:/Library/Frameworks/GStreamer.framework/Versions/1.0/lib/pkgconfig:/usr/local/ssl/lib/pkgconfig:/usr/local/opt/curl/lib/pkgconfig:/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH cmake -DCMAKE_CUSTOM_QTDEMUX_PLUGIN_ENABLED -DCMAKE_OSX_SYSROOT="/" -DCMAKE_OSX_DEPLOYMENT_TARGET="" -DSMOKETEST_ENABLED=ON -G Xcode ../
 
     echo "Please Start XCode, open aamp/build/AAMP.xcodeproj project file"
 	
