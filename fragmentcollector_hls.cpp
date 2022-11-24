ec3fc7c3d6a7b2576f77c05d9e0cbf68e9fb912f
Reshma Raphael <Reshma_Raphael@comcast.com>
2022-11-24 13:02:48 +0000

    DELIA-59118:Webprocess crashed with Peacock SLE and Linear
    Reason for change: Added check to avoid
    wrong value read for next fragment url
    as a part of CMCD.
    Test Procedure: Updated in ticket
    Risks: Low
    
    Signed-off-by: Reshma Raphael <Reshma_Raphael@comcast.com>
    Change-Id: Ib92d623b5c04844d9c73b62f26e06f1c2c913cd7
    (cherry picked from commit 0219e7bbb3efbfcc99d0ca7a52eda686426c2896)

--- fragmentcollector_hls.cpp
+++ fragmentcollector_hls.cpp
@@ -1574,12 +1574,13 @@ char *TrackState::GetNextFragmentUriFromPlaylist(bool ignoreDiscontinuity)
 		}
 		ptr = next;
 		//As a part of RDK-35897 to fetch the url of next next fragment
-		if(ISCONFIGSET(eAAMPConfig_EnableCMCD) && ptr )
+		if((!IsLive()) && (ISCONFIGSET(eAAMPConfig_EnableCMCD)) && (ptr))
 		{
 			const char *CMCDnext = strstr(ptr,"#EXTINF");
 			if( CMCDnext )
 			{
 				const char *nextNew = strstr(CMCDnext+7,"#EXT"); // skip ahead to NEXT fragment entry
+				if(nextNew){
 				const char *foundNew = strchr(nextNew+4,'\n'); // skip fragment duration
 				if( foundNew )
 				{
@@ -1591,7 +1592,7 @@ char *TrackState::GetNextFragmentUriFromPlaylist(bool ignoreDiscontinuity)
 						aamp->mCMCDNextObjectRequest = mCMCDNextObjectRequest;
 						AAMPLOG_INFO("Next fragment url %s",mCMCDNextObjectRequest.c_str());
 					}
-				}
+				}}
 			}
 
 		}
