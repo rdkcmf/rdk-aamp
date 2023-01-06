55d53c8edc1cf4beeea3861f99c21efa292cde69
Shripad Bankar <shripad_bankar@cable.comcast.com>
2023-01-06 16:35:24 +0000

    Revert "RDKTV-21384 [AAMP] Release jobs are failing for spin 5.11p3s1"
    This reverts commit 12a5c271a660d1dc01721d12c0469841e5a4f66a.
    
    Reason for revert: reverting original change as well
    
    Change-Id: I3df92b8b14f879a094461f3ada48ca02f95c4af1

--- AampConfig.cpp
+++ AampConfig.cpp
@@ -481,7 +481,13 @@ void AampConfig::Initialize()
 	bAampCfgValue[eAAMPConfig_EnableCMCD].value				=	false;
 	bAampCfgValue[eAAMPConfig_EnableSlowMotion].value			=	true;
 	bAampCfgValue[eAAMPConfig_EnableSCTE35PresentationTime].value			=	false;
+<<<<<<< HEAD
+=======
+	bAampCfgValue[eAAMPConfig_IgnoreAppLiveOffset].value 			=	false;
+	bAampCfgValue[eAAMPConfig_enableDisconnectSignals].value = true;
+	bAampCfgValue[eAAMPConfig_enableEOSInjectionDuringStop].value = true;
 	bAampCfgValue[eAAMPConfig_JsInfoLogging].value                          = 	false;
+>>>>>>> 7f39e55e (RDKAAMP-410 : Delayed logs and insufficient logs)
 
 	///////////////// Following for Integer Data type configs ////////////////////////////
 	iAampCfgValue[eAAMPConfig_HarvestCountLimit-eAAMPConfig_IntStartValue].value		=	0;
